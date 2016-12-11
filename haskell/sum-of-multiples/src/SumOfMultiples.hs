98d0af4824461d8ab8cc732d217bcd57b7d4a0e4
Robert Hill <dr.pappa@gmail.com>
2016-12-11 21:29:53 +0000

    Haskell - Sum of Multiples

--- haskell/sum-of-multiples/src/SumOfMultiples.hs
+++ haskell/sum-of-multiples/src/SumOfMultiples.hs
@@ -1,3 +1,11 @@
 module SumOfMultiples (sumOfMultiples) where
 
-sumOfMultiples = undefined
+sumOfMultiples :: [Int] -> Int -> Int
+sumOfMultiples [] limit = 0
+sumOfMultiples factors limit = sum multiples
+    where multiples = makeMultiples factors [] limit
+
+makeMultiples :: [Int] -> [Int] -> Int -> [Int] 
+makeMultiples [] output limit = output
+makeMultiples (x:xs) output limit = makeMultiples xs (output ++ [x, xx..limit]) limit
+    where xx = x+x
