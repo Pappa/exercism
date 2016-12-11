217dde7d88549f7fd83be666b601c7605a208edf
Robert Hill <dr.pappa@gmail.com>
2016-12-11 18:06:57 +0000

    Bob Haskell

--- haskell/bob/src/Bob.hs
+++ haskell/bob/src/Bob.hs
@@ -6,7 +6,7 @@ import Data.List
 responseFor :: String -> String
 responseFor str
     | isLastChar str "?"                      = "Sure."
-    | (all isUpper str || isLastChar str "!") = "Whoa, chill out!"
+    | all isUpper (removePunctuation str)     = "Whoa, chill out!"
     | str == ""                               = "Fine. Be that way!"
     | otherwise                               = "Whatever."
 
@@ -15,3 +15,7 @@ isLastChar :: String -> String -> Bool
 isLastChar h n
     | last h == head n = True
     | otherwise        = False
+
+removePunctuation :: String -> String
+removePunctuation str =
+    filter (`elem` ['a'..'z'] ++ ['A'..'Z']) str
