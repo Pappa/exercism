1c40a9e019123c22f5bed01a5fe0a770b41bb109
Pappa <dr.pappa@gmail.com>
2016-12-12 17:11:15 +0000

    Haskell - Grains - WIP

--- haskell/grains/src/Grains.hs
+++ haskell/grains/src/Grains.hs
@@ -1,7 +1,15 @@
 module Grains (square, total) where
 
 square :: Integer -> Maybe Integer
-square n = undefined
+square n
+    | n > 0 && n < 65 = Just ((generateSquares [])!!(fromIntegral n) - 1)
+    | otherwise = Nothing
 
 total :: Integer
-total = undefined
+total = sum $ generateSquares []
+
+generateSquares :: [Integer] -> [Integer]
+generateSquares [] = generateSquares [1]
+generateSquares squares@(x:_)
+    | length squares == 64 = reverse squares
+    | otherwise = generateSquares ([(x * 2)] ++ squares)
