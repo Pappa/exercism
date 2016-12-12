09be8e2610f9bc6599d9a4e7c3b4813b4d04b3e1
Robert Hill <dr.pappa@gmail.com>
2016-12-12 19:58:07 +0000

    Haskell - Grains

--- haskell/grains/src/Grains.hs
+++ haskell/grains/src/Grains.hs
@@ -2,8 +2,9 @@ module Grains (square, total) where
 
 square :: Integer -> Maybe Integer
 square n
-    | n > 0 && n < 65 = Just ((generateSquares [])!!(fromIntegral n) - 1)
+    | n > 0 && n < 65 = Just ((generateSquares [])!!index)
     | otherwise = Nothing
+    where index = (fromIntegral n - 1)
 
 total :: Integer
 total = sum $ generateSquares []
