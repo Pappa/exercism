9aa8b357ab6c5f6f2ef2d4790977c3f9148e6cee
Pappa <dr.pappa@gmail.com>
2016-12-13 09:41:59 +0000

    Haskell - Nucleotide Count - WIP

--- haskell/nucleotide-count/src/DNA.hs
+++ haskell/nucleotide-count/src/DNA.hs
@@ -1,9 +1,13 @@
 module DNA (count, nucleotideCounts) where
 
-import Data.Map (Map)
+import Data.Map
+import Data.List
 
 count :: Char -> String -> Either String Int
 count = undefined
 
 nucleotideCounts :: String -> Either String (Map Char Int)
-nucleotideCounts = undefined
+nucleotideCounts str = Data.Map.fromList $ Prelude.map createTuple $ group $ sort str
+
+createTuple :: (Char, Int)
+createTuple l = (head l, length l)
