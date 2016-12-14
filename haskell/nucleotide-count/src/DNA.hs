bd7f2e62b3c731be487be4d9abe42d177b70520b
Pappa <dr.pappa@gmail.com>
2016-12-14 11:53:46 +0000

    Haskell - Nucleotide Count - WIP

--- haskell/nucleotide-count/src/DNA.hs
+++ haskell/nucleotide-count/src/DNA.hs
@@ -3,31 +3,24 @@ module DNA (count, nucleotideCounts) where
 import Data.Map (Map)
 import qualified Data.Map as Map
 import qualified Data.List as List
+import Data.Maybe (fromJust)
 
 count :: Char -> String -> Either String Int
 count _ [] = Right 0
 count nucleotide strand
-    | nucleotide == 'A' = Right $ 5
+    | value /= Nothing = Right $ (fromJust value)
+    | value == Nothing = Right $ 0
     | otherwise = Left "Bork!"
+    where value :: Maybe Int
+          value = Map.lookup nucleotide (nucleotideCounts strand)
 
 nucleotideCounts :: String -> Either String (Map Char Int)
 nucleotideCounts str
-    | length invalidChars > 0 = Left str
-    | otherwise = Right $ Map.fromList (List.map createTuple $ List.group $ List.sort str)
+    | length invalidChars > 0 = Left "Bork!"
+    | otherwise = Right $ Map.fromList (counts)
     where invalidChars :: String
           invalidChars = List.filter (not . (`elem` "ACGT"))  str
-          --countMap :: [(Char Int)]
-          
---nucleotideCounts str = Map.fromList (List.map createTuple $ group $ sort str)
-
-createTuple :: String -> (Char, Int)
-createTuple l = (head l, length l)
-
---countMap = [ ('A', 0)
--- , ('C', 0)
---, ('G', 0)
--- , ('T', 0) ]
-
+          counts = List.map (\x -> (head x, length x)) . List.group $ List.sort str
 
 
 --Data.Map.lookup
