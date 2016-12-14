d5a3415b2d973ed662c4f3c77c73346810bf4c93
Pappa <dr.pappa@gmail.com>
2016-12-14 17:06:54 +0000

    Haskell - Nucleotide Count - semi-working count

--- haskell/nucleotide-count/src/DNA.hs
+++ haskell/nucleotide-count/src/DNA.hs
@@ -1,4 +1,4 @@
-module DNA ({-|count, -}nucleotideCounts) where
+module DNA (count, nucleotideCounts) where
 
 import Data.Map (Map)
 import qualified Data.Map as Map
@@ -6,17 +6,22 @@ import qualified Data.List as List
 import Data.Either (isLeft)
 
 
-{-|
+
 count :: Char -> String -> Either String Int
 count _ [] = Right 0
 count nucleotide strand
-    | isLeft counts = Left counts
-    | otherwise = Right $ (Map.lookup nucleotide counts)
-    where counts :: Either String (Map Char Int)
-          counts = nucleotideCounts strand
--}
+    | length invalidChars > 0 = Left "Bork!"
+    | otherwise = Right $ length $ filter (==nucleotide) strand
+--    | isLeft counts = Left counts
+--    | otherwise = Right $ (Map.lookup nucleotide counts)
+--    where counts :: Either String (Map Char Int)
+--          counts = nucleotideCounts strand
+    where invalidChars :: String
+          invalidChars = List.filter (not . (`elem` "ACGT"))  strand
+
 nucleotideCounts :: String -> Either String (Map Char Int)
 nucleotideCounts str
+    | length str == 0 = Right $ Map.fromList countMap
     | length invalidChars > 0 = Left "Bork!"
     | otherwise = Right $ Map.fromList $ counts
     where invalidChars :: String
@@ -25,3 +30,4 @@ nucleotideCounts str
           counts = List.map (\x -> (head x, length x)) . List.group $ List.sort str
           countMap :: [(Char, Int)]
           countMap = [ ('A', 0), ('C', 0), ('G', 0), ('T', 0) ]
+--          getCounts :: [(Char, Int)]
