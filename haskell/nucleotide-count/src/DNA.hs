be0c37fe1094fa681648b8e2b269c72962c82189
Pappa <dr.pappa@gmail.com>
2016-12-15 11:54:50 +0000

    Haskell - nucleotide-count

--- haskell/nucleotide-count/src/DNA.hs
+++ haskell/nucleotide-count/src/DNA.hs
@@ -4,27 +4,33 @@ import Data.Map (Map)
 import qualified Data.Map as Map
 import qualified Data.List as List
 import Data.Either (isLeft)
-
+import Data.Either.Utils (fromRight)
 
 
 count :: Char -> String -> Either String Int
 count _ [] = Right 0
 count nucleotide strand
-    | length strand == 0 = Right 0
-    | length invalidChars == 0 = Right $ length $ filter (==nucleotide) strand
-    | otherwise = Left "Bork!"
-    where invalidChars :: String
-          invalidChars = List.filter (not . (`elem` "ACGT"))  strand
+    | not $ (nucleotide `elem` strand) = Left e
+    | length (getInvalidChars strand) > 0 = Left e
+    | otherwise = Right $ length $ filter (==nucleotide) strand
+    where e = "Bork!"
 
 nucleotideCounts :: String -> Either String (Map Char Int)
-nucleotideCounts str
-    | length str == 0 = Right $ Map.fromList countMap
-    | length invalidChars > 0 = Left "Bork!"
+nucleotideCounts strand
+    | length strand == 0 = Right $ Map.fromList countMap
+    | length (getInvalidChars strand) > 0 = Left "Bork!"
     | otherwise = Right $ Map.fromList $ counts
-    where invalidChars :: String
-          invalidChars = List.filter (not . (`elem` "ACGT"))  str
-          counts :: [(Char, Int)]
-          counts = List.map (\x -> (head x, length x)) . List.group $ List.sort str
-          countMap :: [(Char, Int)]
-          countMap = [ ('A', 0), ('C', 0), ('G', 0), ('T', 0) ]
---          getCounts :: [(Char, Int)]
+    where counts :: [(Char, Int)]
+          counts = List.map (\(k, v) -> (getCount k strand)) countMap
+
+getInvalidChars :: String -> String
+getInvalidChars str = List.filter (not . (`elem` "ACGT")) str
+
+countMap :: [(Char, Int)]
+countMap = [ ('A', 0), ('C', 0), ('G', 0), ('T', 0) ]
+
+getCount :: Char -> String -> (Char, Int)
+getCount c str
+    | isLeft ccount = (c, 0)
+    | otherwise = (c, fromRight ccount)
+    where ccount = count c str
