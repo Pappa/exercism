f6462d3b0ae3659902c693a40b5d49e91ec4080f
Robert Hill <dr.pappa@gmail.com>
2016-12-18 17:35:13 +0000

    Haskell - Nucleotide Count - RF

--- haskell/nucleotide-count/src/DNA.hs
+++ haskell/nucleotide-count/src/DNA.hs
@@ -10,7 +10,7 @@ import Data.Either.Utils (fromRight)
 count :: Char -> String -> Either String Int
 count _ [] = Right 0
 count nucleotide strand
-    | not $ (nucleotide `elem` strand) = Left e
+    | isInvalidNucleotide nucleotide = Left e
     | isInvalidStrand strand = Left e
     | otherwise = Right $ length $ filter (==nucleotide) strand
     where e = "Bork!"
@@ -23,7 +23,10 @@ nucleotideCounts strand
           counts = List.map (\k -> (getCount k strand)) nucleotides
 
 isInvalidStrand :: String -> Bool
-isInvalidStrand strand = List.any (not . (`elem` nucleotides)) strand
+isInvalidStrand strand = any (not . (`elem` nucleotides)) strand
+
+isInvalidNucleotide :: Char -> Bool
+isInvalidNucleotide nucleotide = not $ (nucleotide `elem` nucleotides)
 
 nucleotides :: String
 nucleotides = "ACGT"
