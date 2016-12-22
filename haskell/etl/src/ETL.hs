e1fe7d0be6c2190a1288b71ffc627b8f32f0f868
Pappa <dr.pappa@gmail.com>
2016-12-22 12:18:00 +0000

    Haskell - ETL - WIP

--- haskell/etl/src/ETL.hs
+++ haskell/etl/src/ETL.hs
@@ -1,6 +1,12 @@
 module ETL (transform) where
 
 import Data.Map (Map)
+import qualified Data.Map as M
+import Data.Char (toLower)
 
-transform :: Map a String -> Map Char a
-transform = undefined
+transform :: Map Int String -> Map Char Int
+transform input = M.fromList $ fmap transformItem $ M.toList input
+
+transformItem :: (Int, String) -> (Char, Int)
+transformItem (i, s:[]) = (toLower s, i)
+transformItem (i, s:ss) = (toLower s, i)
