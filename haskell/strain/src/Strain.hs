8d6320a9106653af7cd68a3ac54b2980a0f1371a
Robert Hill <dr.pappa@gmail.com>
2016-12-24 12:36:23 +0000

    Haskell - Strain

--- haskell/strain/src/Strain.hs
+++ haskell/strain/src/Strain.hs
@@ -1,7 +1,9 @@
 module Strain (keep, discard) where
 
+--import Data.List (filter)
+
 discard :: (a -> Bool) -> [a] -> [a]
-discard = undefined
+discard f l = filter (not . f) l
 
 keep :: (a -> Bool) -> [a] -> [a]
-keep = undefined
+keep = filter
