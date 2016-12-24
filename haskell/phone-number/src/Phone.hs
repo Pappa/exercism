0335d6c99cf59547ea6af3a9ef23426ae2a9ae96
Robert Hill <dr.pappa@gmail.com>
2016-12-24 14:27:57 +0000

    Haskell - Phone Number

--- haskell/phone-number/src/Phone.hs
+++ haskell/phone-number/src/Phone.hs
@@ -1,10 +1,26 @@
 module Phone (areaCode, number, prettyPrint) where
 
+import Data.Maybe (isJust, fromJust)
+
 areaCode :: String -> Maybe String
-areaCode = undefined
+areaCode phoneNumber
+    | isJust phone = Just (take 3 $ fromJust phone)
+    | otherwise = Nothing 
+    where phone = number phoneNumber
 
 number :: String -> Maybe String
-number = undefined
+number phoneNumber
+    | length phone < 10 = Nothing
+    | length phone == 10 = Just phone
+    | length phone == 11 && (head phone) == '1' = Just (tail phone)
+    | length phone > 10 = Nothing
+    where phone = validNumbers phoneNumber
 
 prettyPrint :: String -> Maybe String
-prettyPrint = undefined
+prettyPrint phoneNumber
+    | isJust phone = Just $ "(" ++ (take 3 $ fromJust phone) ++ ") " ++ (take 3 $ drop 3 $ fromJust phone) ++ "-" ++ (drop 6 $ fromJust phone)
+    | otherwise = Nothing 
+    where phone = number phoneNumber
+
+validNumbers :: String -> String
+validNumbers = filter(`elem` "0123456789")
