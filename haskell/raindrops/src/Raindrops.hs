e60a9fcfb572b7e9f67e332d5e5ba7eba1ebbcea
Robert Hill <dr.pappa@gmail.com>
2016-12-24 10:57:51 +0000

    Haskell - Raindrops

--- haskell/raindrops/src/Raindrops.hs
+++ haskell/raindrops/src/Raindrops.hs
@@ -2,14 +2,12 @@ module Raindrops (convert) where
 
 convert :: Int -> String
 convert input
-    | length raindropsString == 0 = show input
-    | otherwise = raindropsString
+    | length raindropsString > 0 = raindropsString
+    | otherwise = show input
     where raindropsString = concat $ fmap (getString input) raindrops
 
 getString :: Int -> (Int, String) -> String
-getString i (n, s)
-    | i `mod` n == 0 = s
-    | otherwise = ""
+getString i (n, s) = if (i `mod` n == 0) then s else ""
 
 raindrops :: [(Int, String)]
 raindrops = [(3, "Pling"),(5, "Plang"),(7, "Plong")]
