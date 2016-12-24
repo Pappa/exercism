969c1f826d4d05f91293a088bd6d8c1d2135e549
Robert Hill <dr.pappa@gmail.com>
2016-12-24 17:39:20 +0000

    Haskell - Beer Song

--- haskell/beer-song/src/Beer.hs
+++ haskell/beer-song/src/Beer.hs
@@ -1,6 +1,26 @@
 module Beer (song) where
 
 song :: String
+song = concatMap makeLyrics [99, 98..1]
+
+makeLyrics :: Int -> String
+makeLyrics x
+    | x > 2 = show x ++ " bottles of beer on the wall, " ++ show x ++ " bottles of beer.\n\
+       \Take one down and pass it around, " ++ show (x-1) ++ " bottles of beer on the wall.\n\
+       \\n\
+       \"
+    | x == 2 = show x ++ " bottles of beer on the wall, " ++ show x ++ " bottles of beer.\n\
+       \Take one down and pass it around, " ++ show (x-1) ++ " bottle of beer on the wall.\n\
+       \\n\
+       \"
+    | otherwise = "1 bottle of beer on the wall, 1 bottle of beer.\n\
+       \Take it down and pass it around, no more bottles of beer on the wall.\n\
+       \\n\
+       \No more bottles of beer on the wall, no more bottles of beer.\n\
+       \Go to the store and buy some more, 99 bottles of beer on the wall.\n"
+
+{-|
+song :: String
 song = "99 bottles of beer on the wall, 99 bottles of beer.\n\
        \Take one down and pass it around, 98 bottles of beer on the wall.\n\
        \\n\
@@ -301,3 +321,4 @@ song = "99 bottles of beer on the wall, 99 bottles of beer.\n\
        \No more bottles of beer on the wall, no more bottles of beer.\n\
        \Go to the store and buy some more, 99 bottles of beer on the wall.\n"
 
+-}
