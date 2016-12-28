5e03ed46f146e191be9ac4d97b5448bf28e91290
Robert Hill <dr.pappa@gmail.com>
2016-12-28 11:11:10 +0000

    Haskell - Triangle

--- haskell/triangle/src/Triangle.hs
+++ haskell/triangle/src/Triangle.hs
@@ -7,8 +7,30 @@ data TriangleType = Equilateral
                   deriving (Eq, Show)
 
 triangleType a b c
+    | isIllegal a b c = Illegal
     | isEquilateral a b c = Equilateral
+    | isIsosceles a b c = Isosceles
+    | isScalene a b c = Scalene
     | otherwise = Illegal
 
 isEquilateral :: Float -> Float -> Float -> Bool
 isEquilateral a b c = a == b && a == c && a > 0
+
+isIsosceles :: Float -> Float -> Float -> Bool
+isIsosceles a b c
+    | a == b && a /= c = True
+    | a == c && a /=b = True
+    | b == c && b /=a = True
+    | otherwise = False
+
+isScalene :: Float -> Float -> Float -> Bool
+isScalene a b c
+    | a /= b && a /=c && b /= c = True
+    | otherwise = False
+
+isIllegal :: Float -> Float -> Float -> Bool
+isIllegal a b c
+    | (a + b) < c = True
+    | (a + c) < b = True
+    | (b + c) < a = True
+    | otherwise = False
