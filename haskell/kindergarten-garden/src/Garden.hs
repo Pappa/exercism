470b604ab82913f74979f136b7f5b41cc6a353e5
Robert Hill <dr.pappa@gmail.com>
2017-01-02 19:24:26 +0000

    Haskell - Kindergarten Garden

--- haskell/kindergarten-garden/src/Garden.hs
+++ haskell/kindergarten-garden/src/Garden.hs
@@ -18,10 +18,6 @@ data Plant = Clover
 type Garden = Map String [Plant]
 type Student = String
 
-charToPlant :: Char -> Plant
-charToPlant c = plantMap ! c
-    where plantMap = fromList [('C', Clover), ('G', Grass), ('R', Radishes), ('V', Violets)]
-
 defaultStudents = ["Alice", "Bob", "Charlie", "David", "Eve", "Fred", "Ginny", "Harriet", "Ileana", "Joseph", "Kincaid", "Larry"]
 
 defaultGarden :: String -> Garden
@@ -29,8 +25,9 @@ defaultGarden = garden defaultStudents
 
 garden :: [Student] -> String -> Garden
 garden students plantList = fromList $ zip (sort students) plants
-  where [row1, row2] = map (map charToPlant) $ lines plantList
+  where [row1, row2] = map (map (plantMap !)) $ lines plantList
         plants = zipWith (++) (chunksOf 2 row1) (chunksOf 2 row2)
+        plantMap = fromList [('C', Clover), ('G', Grass), ('R', Radishes), ('V', Violets)]
 
 lookupPlants :: Student -> Garden -> [Plant]
 lookupPlants = flip (!)
