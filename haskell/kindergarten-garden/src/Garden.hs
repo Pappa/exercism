44f7884a3bce8c035e8baca7c6f87036506e09de
Robert Hill <dr.pappa@gmail.com>
2017-01-02 11:35:41 +0000

    Haskell - Kindergarten Garden - WIP

---
+++ haskell/kindergarten-garden/src/Garden.hs
@@ -0,0 +1,36 @@
+module Garden
+    ( Plant (..)
+    , defaultGarden
+    , garden
+    , lookupPlants
+    ) where
+
+import Data.Map (Map, fromList, (!))
+import Data.List.Split (chunksOf)
+import Data.List (sort)
+
+data Plant = Clover
+           | Grass
+           | Radishes
+           | Violets
+           deriving (Eq, Show)
+
+type Garden = Map String [Plant]
+type Student = String
+
+charToPlant :: Char -> Plant
+charToPlant c = plantMap ! c
+    where plantMap = fromList [('C', Clover), ('G', Grass), ('R', Radishes), ('V', Violets)]
+
+defaultStudents = ["Alice", "Bob", "Charlie", "David", "Eve", "Fred", "Ginny", "Harriet", "Ileana", "Joseph", "Kincaid", "Larry"]
+
+defaultGarden :: String -> Garden
+defaultGarden = garden defaultStudents
+
+garden :: [Student] -> String -> Garden
+garden students plantList = fromList $ zip (sort students) plants
+  where [row1, row2] = map (map charToPlant) $ lines plantList
+        plants = zipWith (++) (chunksOf 2 row1) (chunksOf 2 row2)
+
+lookupPlants :: Student -> Garden -> [Plant]
+lookupPlants = flip (!)
