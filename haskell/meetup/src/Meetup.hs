b849a50d67e7954b9c2e3ab4555bb2fc854cef36
Pappa <dr.pappa@gmail.com>
2017-01-03 16:30:07 +0000

    Haskell - Meetup - WIP

--- haskell/meetup/src/Meetup.hs
+++ haskell/meetup/src/Meetup.hs
@@ -1,6 +1,6 @@
 module Meetup (Weekday(..), Schedule(..), meetupDay) where
 
-import Data.Time.Calendar (Day)
+import Data.Time.Calendar (Day, fromGregorian, gregorianMonthLength)
 
 data Schedule = First
               | Second
@@ -23,4 +23,26 @@ type Year = Integer
 type Month = Int
 
 meetupDay :: Schedule -> Weekday -> Year -> Month -> Day
-meetupDay schedule weekday year month = undefined
+meetupDay schedule weekday year month = applyNTimes (firstDayOfWeekOffset + weekdayOffset -1) succ firstOfMonth
+    where
+        firstOfMonth = fromGregorian year month 1
+        daysInMonth = gregorianMonthLength year month
+        firstDayOfWeekOffset = case schedule of
+            First -> 1
+            Second -> 8
+            Third -> 15
+            Fourth -> 22
+            Last -> daysInMonth - 7
+            Teenth -> 13
+        weekdayOffset = case weekday of
+            Monday -> 0
+            Tuesday -> 1
+            Wednesday -> 2
+            Thursday -> 3
+            Friday -> 4
+            Saturday -> 5
+            Sunday -> 6
+
+applyNTimes :: Int -> (a -> a) -> a -> a
+applyNTimes 0 _ val = val
+applyNTimes n f val = foldl (\s e -> e s) val [f | x <- [1..n]]
