74daff7af6aa6a6117001756e4107fb6be4552e3
Pappa <dr.pappa@gmail.com>
2017-01-03 12:27:44 +0000

    Haskell - List Ops - WIP

---
+++ haskell/list-ops/src/ListOps.hs
@@ -0,0 +1,45 @@
+module ListOps
+  ( length
+  , reverse
+  , map
+  , filter
+  , foldr
+  , foldl'
+  , (++)
+  , concat
+  ) where
+
+import Prelude hiding
+  ( length, reverse, map, filter, foldr, (++), concat )
+
+foldl' :: (b -> a -> b) -> b -> [a] -> b
+foldl' = undefined
+
+foldr :: (a -> b -> b) -> b -> [a] -> b
+foldr = undefined
+
+length :: [a] -> Int
+length [] = 0
+length xs = sum $ map (\_ -> 1 :: Int) xs
+
+reverse :: [a] -> [a]
+reverse l =  rev l []
+    where
+        rev [] a     = a
+        rev (x:xs) a = rev xs (x:a)
+
+map :: (a -> b) -> [a] -> [b]
+map _ [] = []
+map f (x:xs) = f x : map f xs
+
+filter :: (a -> Bool) -> [a] -> [a]
+filter _ [] = []
+filter p (x:xs)
+    | p x = x : filter p xs
+    | otherwise = filter p xs
+
+(++) :: [a] -> [a] -> [a]
+xs ++ ys = undefined
+
+concat :: [[a]] -> [a]
+concat = undefined
