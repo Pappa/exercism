637d68a3dabb2e9bb4473ba46ca0a11a8e880718
Robert Hill <dr.pappa@gmail.com>
2017-01-07 17:11:27 +0000

    Haskell - Largest Series Product

---
+++ haskell/largest-series-product/src/Series.hs
@@ -0,0 +1,23 @@
+module Series (largestProduct) where
+
+import Data.Char (digitToInt, isDigit)
+import qualified Data.List as L (product)
+
+largestProduct :: Int -> String -> Maybe Int
+largestProduct 0 _ = Just 1
+largestProduct span str
+    | span < 2 = Nothing
+    | length str < span = Nothing
+    | not $ all isDigit str = Nothing
+    | otherwise = Just $ largestProduct' span str 0
+
+largestProduct' :: Int -> String -> Int -> Int
+largestProduct' span str@(x:xs) pr
+    | length str == span = l
+    | otherwise = largestProduct' span xs l
+    where
+    	p = prod $ take span str
+    	l = if p > pr then p else pr
+
+prod :: String -> Int
+prod str = L.product $ map digitToInt str
