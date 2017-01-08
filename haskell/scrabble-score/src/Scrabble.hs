21467e56d0724b289c6bc05de5e2fc7b71e77a97
Robert Hill <dr.pappa@gmail.com>
2017-01-08 17:03:45 +0000

    Haskell - Scrabble Score - O(log n) version

--- haskell/scrabble-score/src/Scrabble.hs
+++ haskell/scrabble-score/src/Scrabble.hs
@@ -1,20 +1,20 @@
 module Scrabble (scoreLetter, scoreWord) where
 
-import Data.List (find)
+import qualified Data.Map as M (Map, fromList, lookup)
 import Data.Maybe (isNothing, fromJust)
 import Data.Char (toUpper)
 
 scoreLetter :: Char -> Int
 scoreLetter letter
     | isNothing score = 0
-    | otherwise = snd $ fromJust score
-    where score = find (\t -> (toUpper letter) `elem` (fst t)) scores
+    | otherwise = fromJust score
+    where score = M.lookup (toUpper letter) scores
 
 scoreWord word = sum $ fmap scoreLetter word
 
-scores :: [(String, Int)]
-scores = [
-    ("AEIOULNRST", 1),
+scores :: M.Map Char Int
+scores = M.fromList $ concat $ map (\(str,score) -> (map (\s -> (s, score)) str)) [
+    ("AEIOULNRST", 1 :: Int),
     ("DG", 2),
     ("BCMP", 3),
     ("FHVWY", 4),
