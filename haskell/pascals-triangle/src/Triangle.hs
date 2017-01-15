f800f51f6421339ba5ecaa818e626f1fb024b87d
Robert Hill <dr.pappa@gmail.com>
2017-01-15 19:53:10 +0000

    Haskell - Pascal's Triangle

--- haskell/pascals-triangle/src/Triangle.hs
+++ haskell/pascals-triangle/src/Triangle.hs
@@ -1,18 +1,7 @@
 module Triangle (rows) where
 
 rows :: Int -> [[Integer]]
-rows n
-    | n < 1 = []
-    | otherwise = addRow n [[1]]
-
-addRow :: Int -> [[Integer]] -> [[Integer]]
-addRow n ls@(x:xs)
-    | length ls == n = reverse ls
-    | otherwise = (addRow n ((createRow (expandRow x) []) : ls))
-
-expandRow :: [Integer] -> [Integer]
-expandRow x = 0 : x ++ [0]
-
-createRow :: [Integer] -> [Integer] -> [Integer]
-createRow (x1:[]) output = output
-createRow (x1:x2:xs) output = createRow (x2:xs) ((x1 + x2):output)
+rows n 
+    | n <= 0 = []
+    | otherwise = reverse $ foldr (.) id (replicate (n-1) rows') [[1]]
+    where rows' l@(x:xs) = (zipWith (+) (0:x) (x++[0])):l
