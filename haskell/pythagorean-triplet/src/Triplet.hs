8067a01debaecfcec1129ec1177f8b62d6f441e3
Robert Hill <dr.pappa@gmail.com>
2017-01-18 07:38:29 +0000

    Haskell - Pythagorean Triplet

---
+++ haskell/pythagorean-triplet/src/Triplet.hs
@@ -0,0 +1,15 @@
+module Triplet (isPythagorean, mkTriplet, pythagoreanTriplets) where
+
+import Data.Function (on)
+import Data.List (sortBy)
+
+isPythagorean (a, b, c)
+    | a**2 + b**2 == c**2 = True
+    | b**2 + c**2 == a**2 = True
+    | a**2 + c**2 == b**2 = True
+    | otherwise = False
+
+mkTriplet a b c = (a, b, c)
+
+pythagoreanTriplets x y = sortBy (compare `on` f) $ [(a,b,c) | c <- [x..y], b <- [x..c], a <- [x..b], a^2 + b^2 == c^2]
+    where f (x, _, _) = x
