cc44b750d01fe8fb615e969e98af685b97d558fa
Robert Hill <dr.pappa@gmail.com>
2017-02-05 20:26:09 +0000

    Exercism - Javascript - RNA Transcription

---
+++ javascript/rna-transcription/rna-transcription.js
@@ -0,0 +1,24 @@
+var nucleotides = "GCTA".split("");
+
+var nucleotideMap = {
+	G: "C",
+	C: "G",
+	T: "A",
+	A: "U"
+};
+
+function isValid(nucleotideArray) {
+	return nucleotideArray.every((n) => nucleotides.includes(n));
+}
+
+function DnaTranscriber() {};
+
+DnaTranscriber.prototype.toRna = (str) => {
+	var nucleotideArray = str.split("");
+	if (isValid(nucleotideArray)) {
+		return nucleotideArray.map((n) => nucleotideMap[n]).join();
+	}
+	throw new Error('Invalid input');
+};
+
+module.exports = DnaTranscriber;
