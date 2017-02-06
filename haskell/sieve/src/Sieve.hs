d4cd001210d9a8a277f69c182cff4df7aa2a524e
Robert Hill <dr.pappa@gmail.com>
2017-02-06 18:30:46 +0000

    Haskell - Sieve of Eratosthenes

--- haskell/sieve/src/Sieve.hs
+++ haskell/sieve/src/Sieve.hs
@@ -4,9 +4,9 @@ primesUpTo :: Integer -> [Integer]
 primesUpTo x
     | x < 2 = []
     | otherwise = seive [2..x] []
-    where 
-		seive :: [Integer] -> [Integer] -> [Integer]
-		seive [] acc = acc
-		seive xxs@(x:xs) acc = seive seived (acc ++ [x])
-			where
-				seived = filter ((/=0) . (`mod` x)) xxs
+    
+seive :: [Integer] -> [Integer] -> [Integer]
+seive [] acc = acc
+seive xxs@(x:xs) acc = seive seived (acc ++ [x])
+    where
+        seived = filter ((/=0) . (`mod` x)) xxs
