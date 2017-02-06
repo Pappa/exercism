bc2c0f6899519e339ab49c0751298108f11b956b
Robert Hill <dr.pappa@gmail.com>
2017-02-06 19:42:23 +0000

    Javascript - Word Count

--- javascript/word-count/word-count.js
+++ javascript/word-count/word-count.js
@@ -7,9 +7,9 @@ Words.prototype.count = (str) => {
 		.map(stripQuotes)
 		.filter(w => w !== '');
 	return words.reduce((acc, word) => {
-		acc[word] = Number.isInteger(acc[word]) ? acc[word] + 1 : 1;
+		acc[word] = acc[word] ? acc[word] + 1 : 1;
 		return acc;
-	}, {});
+	}, Object.create(null));
 };
 
 var stripQuotes = function(word) {
