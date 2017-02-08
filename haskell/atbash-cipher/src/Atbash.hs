d9832b394b4e65cae6c309e296481ec7d2418e55
Robert Hill <dr.pappa@gmail.com>
2017-02-08 19:56:38 +0000

    Haskell - Atbash

--- haskell/atbash-cipher/src/Atbash.hs
+++ haskell/atbash-cipher/src/Atbash.hs
@@ -1,8 +1,8 @@
 module Atbash (decode, encode) where
 
-import Data.Char (toLower)
+import Data.Char (toLower, isAlphaNum)
 import qualified Data.Map as M (Map, fromList, lookup)
-import Data.Maybe (catMaybes)
+import Data.Maybe (mapMaybe)
 import Data.List.Split (chunksOf)
 
 letters = ['a'..'z']
@@ -11,9 +11,9 @@ chars = letters ++ numbers
 atbash = M.fromList $ zip chars $ (reverse letters) ++ numbers
 
 decode :: String -> String
-decode input = catMaybes $ map (`M.lookup` atbash) filtered
+decode input = mapMaybe (`M.lookup` atbash) filtered
   where
-    filtered = filter (`elem` chars) $ map toLower input
+    filtered = filter isAlphaNum $ map toLower input
 
 encode :: String -> String
 encode input = unwords $ chunksOf 5 $ decode input
