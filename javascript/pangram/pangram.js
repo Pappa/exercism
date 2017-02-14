3769a1514cc231164e20afad5cdc27c77a48f378
Robert Hill <dr.pappa@gmail.com>
2017-02-14 18:59:19 +0000

    Javascript - Pangram

--- javascript/pangram/pangram.js
+++ javascript/pangram/pangram.js
@@ -4,16 +4,18 @@ function Pangram(word) {
         .toLowerCase()
         .split('')
         .sort()
-        .reduce((acc, letter) => {
-            if (!acc.includes(letter)) {
-                acc.push(letter);
-            }
-            return acc;
-        }, []);
+        .reduce(removeRepeats, []);
 };
 
 Pangram.prototype.isPangram = function() {
     return this.letters.length === 26;
 };
+
+function removeRepeats(acc, letter) {
+    if (!acc.includes(letter)) {
+        acc.push(letter);
+    }
+    return acc;
+}
 
 module.exports = Pangram;
