b0cc714cf13c37bc0bc68c03cccf3581d9cb4757
Robert Hill <dr.pappa@gmail.com>
2017-05-04 21:04:27 +0100

    Haskell - Queen Attack

--- haskell/queen-attack/src/Queens.hs
+++ haskell/queen-attack/src/Queens.hs
@@ -2,19 +2,19 @@ module Queens (boardString, canAttack) where
 
 import Data.List(intersperse)
 
+type Queen = (Int, Int)
+
 board = replicate 8 $ replicate 8 '_'
 
-boardString :: Maybe (Int, Int) -> Maybe (Int, Int) -> String
-boardString Nothing Nothing = format board
-boardString (Just w) Nothing = format $ place w 'W' board
-boardString Nothing (Just b) = format $ place b 'B' board
-boardString (Just w) (Just b) = format $ place w 'W' $ place b 'B' board
+boardString :: Maybe Queen -> Maybe Queen -> String
+boardString w b = format $ place w 'W' $ place b 'B' board
 
 format :: [String] -> String
 format board = unlines $ map (intersperse ' ') board
 
-place :: (Int, Int) -> Char -> [String] -> [String]
-place (r, c) q board = newBoard
+place :: Maybe Queen -> Char -> [String] -> [String]
+place Nothing _ board = board
+place (Just (r, c)) q board = newBoard
     where
         (rb, (row:ra)) = splitAt r board
         (cb, (col:ca)) = splitAt c row
@@ -22,7 +22,7 @@ place (r, c) q board = newBoard
         newBoard = rb ++ [newRow] ++ ra
 
 
-canAttack :: (Int, Int) -> (Int, Int) -> Bool
+canAttack :: Queen -> Queen -> Bool
 canAttack (ar, ac) (br, bc)
     | ar == br || ac == bc = True
     | abs (ar - br) == abs (ac - bc) = True
