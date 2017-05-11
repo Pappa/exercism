e46317822553f8bfb94b52d4bc8e88e4db1a36b1
Robert Hill <dr.pappa@gmail.com>
2017-05-11 07:02:30 +0100

    Python - Hamming Distance

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,11 +1,9 @@
 def distance(strand1, strand2):
-	n1 = list(strand1)
-	n2 = list(strand2)
 	acc = 0
 
-	if len(n1) == len(n2):
-		for index in range(len(n1)):
-			if n1[index] != n2[index]:
+	if len(strand1) == len(strand2):
+		for n1, n2 in zip(strand1, strand2):
+			if n1 != n2:
 				acc += 1
 		return acc
 	else:
