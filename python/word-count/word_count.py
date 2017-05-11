d75491e376cf461b11ff2c729399a0ac765ecdab
Pappa <dr.pappa@gmail.com>
2017-05-11 12:19:08 +0100

    Python - Word Count

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -1,14 +1,7 @@
-import re
+from collections import Counter
 
 def word_count(input):
-    word_map = {}
-    str = re.sub(r"[!&@$%^&:.?]+", "", input)
-    str = re.sub(r"[,_]", " ", str)
-    words = str.lower().split()
-    for word in words:
-        if word in word_map:
-            word_map[word] += 1
-        else:
-            word_map[word] = 1
-    return word_map
+    str = "".join([c if c.isalnum() else ' ' for c in input.lower()])
+    words = str.split()
+    return dict(Counter(words))
 
