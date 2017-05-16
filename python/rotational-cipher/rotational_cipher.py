5455c421fb1c184472d37c392f373f4e350808f0
Robert Hill <dr.pappa@gmail.com>
2017-05-16 20:03:23 +0100

    Python - Rotational Cypher

---
+++ python/rotational-cipher/rotational_cipher.py
@@ -0,0 +1,16 @@
+import string
+from re import sub
+
+lower = string.ascii_lowercase
+upper = string.ascii_uppercase
+
+def rotate(letters, rotation):
+	mapped = [__map(l, rotation) if l.isalpha() else l for l in letters]
+	return ''.join(mapped)
+
+def __map(letter, rotation):
+	alphabet = (lower, upper)[letter.isupper()]
+	for i, c in enumerate(alphabet):
+		if c == letter:
+			index = (i + rotation) % len(alphabet)
+			return alphabet[index]
