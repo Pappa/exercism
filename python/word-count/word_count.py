28d04e5069dd4e7f69f101bbb05fa4f4cee2cbc1
Robert Hill <dr.pappa@gmail.com>
2017-05-16 18:42:59 +0100

    Python - Word Count

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -1,7 +1,6 @@
 from collections import Counter
 
-def word_count(input):
-    str = "".join([c if c.isalnum() else ' ' for c in input.lower()])
-    words = str.split()
-    return dict(Counter(words))
+def word_count(txt):
+    txt = "".join([c if c.isalnum() else ' ' for c in txt.lower()])
+    return Counter(txt.split())
 
