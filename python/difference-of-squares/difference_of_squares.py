09cc04b01071c786864031356efcb9d0ed86f88d
Robert Hill <dr.pappa@gmail.com>
2017-05-16 20:37:49 +0100

    Python - Difference of Squares

---
+++ python/difference-of-squares/difference_of_squares.py
@@ -0,0 +1,17 @@
+def square_of_sum(num):
+	nums = range(1, num + 1)
+	return __square(sum(nums))
+
+
+def sum_of_squares(num):
+	nums = range(1, num + 1)
+	return sum(map(__square, nums))
+
+
+def difference(num):
+	a = square_of_sum(num)
+	b = sum_of_squares(num)
+	return abs(a - b)
+
+def __square(x):
+	return x ** 2
