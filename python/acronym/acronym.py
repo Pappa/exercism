92ad74390632cf55d24c34808a51eeafa2666cca
Robert Hill <dr.pappa@gmail.com>
2017-05-18 19:10:01 +0100

    Python - Acronym

--- python/acronym/acronym.py
+++ python/acronym/acronym.py
@@ -1,6 +1,4 @@
-import re
+from re import findall
 
 def abbreviate(txt):
-    txt = re.sub(r'([a-z])([A-Z])', r'\1 \2', txt)
-    txt = re.sub(r'(?i)[^a-z]', ' ', txt) 
-    return ''.join([c[0].upper() for c in txt.split()])
+    return ''.join(c.upper() for c in findall(r'(?<![A-Z])[A-Z]|(?<=\W)\w', txt))
