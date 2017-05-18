b490584152f55cc969498aef64e422fde1bb9335
Robert Hill <dr.pappa@gmail.com>
2017-05-18 18:23:20 +0100

    Python - Acronym

--- python/acronym/acronym.py
+++ python/acronym/acronym.py
@@ -2,5 +2,5 @@ import re
 
 def abbreviate(txt):
     txt = re.sub(r'([a-z])([A-Z])', r'\1 \2', txt)
-    txt = re.sub(r'(?i)[^a-z]', r' ', txt) 
+    txt = re.sub(r'(?i)[^a-z]', ' ', txt) 
     return ''.join([c[0].upper() for c in txt.split()])
