02155ab90b73bdba397ad6b6ad1f7d6be3a37aa7
Robert Hill <dr.pappa@gmail.com>
2017-05-18 20:08:53 +0100

    Python - Largest Series Product

---
+++ python/largest-series-product/largest_series_product.py
@@ -0,0 +1,20 @@
+def largest_product(series, span):
+	if span > len(series) or span < 0:
+		raise ValueError
+
+	ints = [int(c) for c in series]
+	products = []
+
+	for idx in range(0, len(ints)):
+		if len(ints) - idx >= span:
+			products.append(ints[idx:span+idx])
+
+	products = map(lambda l: reduce(__prod, l, 1), products)
+
+	if (len(products)) > 0:
+		return max(products)
+	return 1
+
+
+def __prod(x, y):
+	return x * y
