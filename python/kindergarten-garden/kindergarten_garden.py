352082bfb63f1a1920a0fea17f27c6c4723cf7fd
Robert Hill <dr.pappa@gmail.com>
2017-05-19 15:38:45 +0100

    Python - Kindergarten Garden

---
+++ python/kindergarten-garden/kindergarten_garden.py
@@ -0,0 +1,19 @@
+default_students = ["Alice", "Bob", "Charlie", "David", "Eve", "Fred", "Ginny", "Harriet", "Ileana", "Joseph", "Kincaid", "Larry"]
+plants = {
+	'C': 'Clover',
+	'G': 'Grass',
+	'R': 'Radishes',
+	'V': 'Violets'
+}
+
+class Garden(object):
+    def __init__(self, garden, students=default_students):
+    	students.sort()
+    	g = garden.split()
+        self._garden = { students[x/2]:self._get_plants(g, x) for x in range(0, len(g[0]), 2) }
+
+    def _get_plants(self, g, x):
+    	return [plants[g[0][x]], plants[g[0][x+1]], plants[g[1][x]], plants[g[1][x+1]]]
+
+    def plants(self, student):
+    	return self._garden[student]
