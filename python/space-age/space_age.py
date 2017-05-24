649483dc341e3bd91abb9435d4a492e8c8338489
Robert Hill <dr.pappa@gmail.com>
2017-05-24 20:15:44 +0100

    Python - Space Age

---
+++ python/space-age/space_age.py
@@ -0,0 +1,43 @@
+class SpaceAge(object):
+    def __init__(self, age):
+        self.seconds = age
+        self._multipliers = {
+            'on_mercury': 0.2408467,
+            'on_venus': 0.61519726,
+            'on_earth': 1.0,
+            'on_mars': 1.8808158,
+            'on_jupiter': 11.862615,
+            'on_saturn': 29.447498,
+            'on_uranus': 84.016846,
+            'on_neptune': 164.79132
+        };
+
+    def _period(self, planet):
+    	return 31557600 * self._multipliers[planet]
+
+    def _get_age(self, planet):
+    	return float(format(self.seconds / self._period(planet), '.2f'))
+
+    def on_mercury(self):
+        return self._get_age('on_mercury')
+
+    def on_venus(self):
+        return self._get_age('on_venus')
+
+    def on_earth(self):
+        return self._get_age('on_earth')
+
+    def on_mars(self):
+        return self._get_age('on_mars')
+
+    def on_jupiter(self):
+        return self._get_age('on_jupiter')
+
+    def on_saturn(self):
+        return self._get_age('on_saturn')
+
+    def on_uranus(self):
+        return self._get_age('on_uranus')
+
+    def on_neptune(self):
+        return self._get_age('on_neptune')
