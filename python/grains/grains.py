170b57e09a11bdfc84a2e5b50b5eafc414575754
Robert Hill <dr.pappa@gmail.com>
2017-05-25 07:44:18 +0100

    Python - Grains

--- python/grains/grains.py
+++ python/grains/grains.py
@@ -1,11 +1,13 @@
+squares = [ 2 ** x for x in range(64) ]
+
 def on_square(square):
     _raise_if_invalid(square)
-    return 2 ** (square - 1)
+    return squares[square - 1]
 
 def total_after(square):
     _raise_if_invalid(square)
-    return sum([ 2 ** x for x in range(0, square) ])
+    return sum(squares[0:square])
 
 def _raise_if_invalid(square):
-	if not square in range(1, 65):
-		raise ValueError('Square out of range')
+    if not square in range(1, 65):
+	    raise ValueError('Square out of range')
