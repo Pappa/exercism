317d61683a03917be34e26a43d8e5eee316ae04f
Robert Hill <dr.pappa@gmail.com>
2017-05-25 20:59:50 +0100

    Python - Luhn

--- python/luhn/luhn.py
+++ python/luhn/luhn.py
@@ -2,11 +2,11 @@ from re import sub
 
 class Luhn(object):
     def __init__(self, text):
-    	self._valid = False
+        self._valid = False
         if self._is_invalid_string(text):
             return
         if self._checksum(text) == 0:
-        	self._valid = True
+            self._valid = True
 
     def is_valid(self):
         return self._valid
@@ -15,8 +15,8 @@ class Luhn(object):
         return len(sub(r"[0-9 ]", "", text)) > 0 or len(text) < 2
 
     def _checksum(self, text):
-    	numbers = [int(x) for x in sub(r"[^0-9]", "", text)]
-    	odd = numbers[-1::-2]
-    	even = numbers[-2::-2]
-    	total = sum(odd) + sum([int(x) for n in even for x in str(2 * n)])
-    	return total % 10
+        numbers = [int(x) for x in sub(r"[^0-9]", "", text)]
+        odd = numbers[-1::-2]
+        even = numbers[-2::-2]
+        total = sum(odd) + sum([int(x) for n in even for x in str(2 * n)])
+        return total % 10
