4aea957d18139ae3a292214adf0a5c72b0b50c78
Robert Hill <dr.pappa@gmail.com>
2017-05-25 20:00:22 +0100

    Python - Roman Numerals

--- python/roman-numerals/roman_numerals.py
+++ python/roman-numerals/roman_numerals.py
@@ -1,26 +1,26 @@
 def numeral(arabic):
     acc = ''
     while arabic:
-        for n, symbol, duplicate in numerals:
-            d, _ = ((1, None), divmod(arabic, n))[duplicate]
+        for n, symbol in numerals:
+            dup, _ = ((1, None), divmod(arabic, n))[len(symbol) > 1]
             if n <= arabic:
-                acc += symbol * d
-                arabic -= n * d
+                acc += symbol * dup
+                arabic -= n * dup
                 break
     return acc
 
 numerals = [
-    (1000, 'M', True),
-    (900, 'CM', False),
-    (500, 'D', False),
-    (400, 'CD', False),
-    (100, 'C', True),
-    (90, 'XC', False),
-    (50, 'L', False),
-    (40, 'XL', False),
-    (10, 'X', True),
-    (9, 'IX', False),
-    (5, 'V', False),
-    (4, 'IV', False),
-    (1, 'I', True)
+    (1000, 'M'),
+    (900, 'CM'),
+    (500, 'D'),
+    (400, 'CD'),
+    (100, 'C'),
+    (90, 'XC'),
+    (50, 'L'),
+    (40, 'XL'),
+    (10, 'X'),
+    (9, 'IX'),
+    (5, 'V'),
+    (4, 'IV'),
+    (1, 'I')
 ]
