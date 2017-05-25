2f0255f1aa827a88e29cbf899b14937b74e0dbc1
Robert Hill <dr.pappa@gmail.com>
2017-05-25 19:46:58 +0100

    Python - Roman Numerals

--- python/roman-numerals/roman_numerals.py
+++ python/roman-numerals/roman_numerals.py
@@ -1,23 +1,25 @@
-numerals = [(1000, 'M'), (500, 'D'), (100, 'C'), (50, 'L'), (10, 'X'), (5, 'V'), (1, 'I')]
-exceptions = [(900, 999, 'CM'), (400, 499, 'CD'), (90, 99, 'XC'), (40, 49, 'XL'), (9, 9, 'IX'), (4, 4, 'IV')]
-
 def numeral(arabic):
     acc = ''
-    while arabic > 0:
-        if _is_exception(arabic):
-            for low, high, symbol in exceptions:
-                if arabic >= low and arabic <= high:
-                    arabic -= low
-                    acc += symbol
-                    break
-        else:
-            for n, symbol in numerals:
-                d, m = divmod(arabic, n)
-                if d > 0:
-                    acc += symbol * d
-                    arabic -= n * d
-                    break
+    while arabic:
+        for n, symbol in numerals:
+            if n <= arabic:
+                acc += symbol
+                arabic -= n
+                break
     return acc
 
-def _is_exception(arabic):
-    return next((True for low, high, _ in exceptions if arabic in range(low, high + 1)), False)
+numerals = [
+    (1000, 'M'),
+    (900, 'CM'),
+    (500, 'D'),
+    (400, 'CD'),
+    (100, 'C'),
+    (90, 'XC'),
+    (50, 'L'),
+    (40, 'XL'),
+    (10, 'X'),
+    (9, 'IX'),
+    (5, 'V'),
+    (4, 'IV'),
+    (1, 'I')
+]
