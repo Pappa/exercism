d2dcdd1808009eedfb0f0c519bb64fa5c46b923e
Robert Hill <dr.pappa@gmail.com>
2017-05-26 07:13:51 +0100

    Python - Series

--- python/series/series.py
+++ python/series/series.py
@@ -3,6 +3,5 @@ def slices(series, span):
     if length < span or span == 0:
         raise ValueError
     else:
-        out = []
         ints = [int(i) for i in series]
         return [ints[i:span+i] for i in range(length + 1 - span)]
