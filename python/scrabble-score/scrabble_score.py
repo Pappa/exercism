3dcdd43de81e3cb67a99eabecf84dc8376d261a1
Robert Hill <dr.pappa@gmail.com>
2017-05-28 17:58:27 +0100

    Python - Scrabble Score

---
+++ python/scrabble-score/scrabble_score.py
@@ -0,0 +1,12 @@
+scores = { c: i for chars, i in [
+    ("AEIOULNRST", 1),
+    ("DG", 2),
+    ("BCMP", 3),
+    ("FHVWY", 4),
+    ("K", 5),
+    ("JX", 8),
+    ("QZ", 10)
+] for c in chars }
+
+def score(chars):
+    return sum([scores[c.upper()] for c in chars])
