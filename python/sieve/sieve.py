0a0e07b963e046dddea49a909adb0e2c1c653885
Robert Hill <dr.pappa@gmail.com>
2017-06-07 18:33:59 +0100

    Python - Sieve of Eratosthenes

--- python/sieve/sieve.py
+++ python/sieve/sieve.py
@@ -1,11 +1,10 @@
 def sieve(num):
-	numbers = range(2, num + 1)
-	return _sieve(numbers, [])
-
-def _sieve(numbers, acc):
-	if len(numbers) == 0:
-		return acc;
-	head, tail = numbers[0], numbers[1:]
-	sieved = [n for n in tail if n % head != 0]
-	acc.append(head)
-	return _sieve(sieved, acc)
+    limit = num + 1
+    multiples = set()
+    primes = []
+    for n in range(2, limit):
+        if n not in multiples:
+            primes.append(n)
+            for multiple in range(n ** 2, limit, n):
+                multiples.add(multiple)
+    return primes
