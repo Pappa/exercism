5fa4b69118f824286b16cfffe07d544460c26b5b
Robert Hill <dr.pappa@gmail.com>
2017-06-07 21:49:28 +0100

    Python - Nth Prime

--- python/nth-prime/nth_prime.py
+++ python/nth-prime/nth_prime.py
@@ -1,19 +1,17 @@
 def nth_prime(n):
-    if n > 0:
-    	candidate = 2
-    	primes = []
-    	while len(primes) < n:
-    		if is_prime(candidate):
-    			primes.append(candidate)
-    		candidate += 1
-    	return primes.pop()
-    else:
-    	raise ValueError('Value out of range')
+	if n < 1: raise ValueError
+	candidate = 2
+	primes = []
+	while len(primes) < n:
+		if is_prime(candidate):
+			primes.append(candidate)
+		candidate += 1
+	return primes.pop()
 
 
 def is_prime(n):
 	for i in [2, 3, 5, 7, 9]:
 		if (n % i == 0):
 			return n == i
-	return all(False for i in range(11, n) if (n % i == 0))
+	return all(False for i in range(11, n, 2) if (n % i == 0))
 
