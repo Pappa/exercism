a2bdfebbe4b6cabc1a4d74670aec86d523ed4e51
Robert Hill <dr.pappa@gmail.com>
2017-06-08 20:58:03 +0100

    Python - Perfect Numbers

--- python/perfect-numbers/perfect_numbers.py
+++ python/perfect-numbers/perfect_numbers.py
@@ -1,11 +1,11 @@
 from math import sqrt
 
 def divisor_generator(n):
-    for i in range(1, int(sqrt(n) + 1)):
+    yield 1
+    for i in range(2, int(sqrt(n) + 1)):
         if n % i == 0:
             yield i
-            if i * i != n and n / i != n:
-                yield n / i
+            yield n / i
 
 def is_perfect(n):
     return n == sum(divisor_generator(n))
