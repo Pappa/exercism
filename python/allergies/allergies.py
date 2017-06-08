07bbc713d655922421c33c365947d123d0d623a0
Robert Hill <dr.pappa@gmail.com>
2017-06-08 07:02:45 +0100

    Python - Allergies

--- python/allergies/allergies.py
+++ python/allergies/allergies.py
@@ -1,5 +1,3 @@
-from itertools import compress
-
 class Allergies(object):
     def __init__(self, x):
         allergies = ['cats', 'pollen', 'chocolate', 'tomatoes', 'strawberries', 'shellfish', 'peanuts', 'eggs']
@@ -9,7 +7,7 @@ class Allergies(object):
         bits = [int(i) for i in str(bin(x))[2:]]
         bits = ([0] * (8 - len(bits))) + bits
 
-        self.lst = [a[1] for a in zip(bits, allergies) if a[0] == 1]
+        self.lst = [allergy for bit, allergy in zip(bits, allergies) if bit]
 
     def is_allergic_to(self, item):
         return item in self.lst
