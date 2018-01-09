6f34ceee313cf8cbbd34297d5056a82ef8c99cd5
Pappa <dr.pappa@gmail.com>
2018-01-09 12:37:27 +0000

    Javascript - Simple Cipher

--- javascript/simple-cipher/simple-cipher.js
+++ javascript/simple-cipher/simple-cipher.js
@@ -3,21 +3,51 @@ const alphabet = 'abcdefghijklmnopqrstuvwxyz';
 class Cipher {
 
 
-    constructor() {
+    constructor(key) {
+        if (key === '') {
+            throw new Error('Bad key');
+        }
+        if (key && !this.isLowerCase(key)) {
+            throw new Error('Bad key');
+        }
+        if (key) {
+            this.key = key;
+            return;
+        }
         this.generateKey();
     }
 
     encode(str) {
-
+        return str.split('')
+            .map((char, index) => {
+                let newIndex = alphabet.indexOf(char) + alphabet.indexOf(this.key[index]);
+                if (newIndex >= alphabet.length) {
+                    newIndex -= alphabet.length;
+                }
+                return alphabet[newIndex];
+            })
+            .join('');
     }
 
     decode(str) {
-
+        return str.split('')
+            .map((char, index) => {
+                let newIndex = alphabet.indexOf(char) - alphabet.indexOf(this.key[index]);
+                if (newIndex < 0) {
+                    newIndex += alphabet.length;
+                }
+                return alphabet[newIndex];
+            })
+            .join('');
     }
 
     generateKey() {
         this.key = this.key || alphabet;
         this.key = this.key.split('').map((a) => [Math.random(),a]).sort((a,b) => a[0]-b[0]).map((a) => a[1]).join('');
+    }
+
+    isLowerCase(key) {
+        return key.split('').every(l => alphabet.includes(l));
     }
 }
 
