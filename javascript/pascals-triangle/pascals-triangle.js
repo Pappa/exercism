371759f1345838974270523aad8c563c4fed3634
Pappa <dr.pappa@gmail.com>
2018-02-01 15:38:20 +0000

    Javascript - Pascals Triangle

--- javascript/pascals-triangle/pascals-triangle.js
+++ javascript/pascals-triangle/pascals-triangle.js
@@ -1,30 +1,39 @@
 class Triangle {
 
     constructor(n) {
+        this.fillRows(n);
+        this.rows = this.calculateRows(2);
+        this.lastRow = this.rows[this.rows.length - 1];
+    }
+
+    fillRows(n) {
         this.rows = Array(n)
             .fill([])
             .map((row, index) => {
                 return Array(index + 1).fill(1);
-            })
-            .map((row, index, rows) => {
-                if (index === 0) {
-                    return row;
-                }
-                return row.map((n, colIndex, cols) => {
-                    let prev = rows[index - 1];
-                    if (colIndex === 0) {
-                        return prev[colIndex];
-                    }
-                    if (colIndex === cols.length - 1) {
-                        return prev[colIndex - 1];
-                    }
-                    return prev[colIndex - 1] + prev[colIndex];
-                });
             });
-        console.log(this.rows);
-        this.lastRow = this.rows[this.rows.length - 1];
     }
 
+    calculateRows(row) {
+        if (!this.rows[row]) {
+            return this.rows;
+        }
+
+        this.rows[row] = this.rows[row]
+            .map((num, idx) => {
+                let prev = this.rows[row - 1];
+                if (idx === 0) {
+                    return prev[idx];
+                }
+                if (!prev[idx]) {
+                    return prev[idx - 1];
+                }
+                return prev[idx - 1] + prev[idx];
+            });
+
+        return this.calculateRows(row + 1);
+
+    }
 
 }
 
