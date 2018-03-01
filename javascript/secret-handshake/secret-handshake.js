c756956d82908f719f06ab369645e61adc6c3c8d
Pappa <dr.pappa@gmail.com>
2018-03-01 16:54:38 +0000

    Javascript - Secret Handshake

--- javascript/secret-handshake/secret-handshake.js
+++ javascript/secret-handshake/secret-handshake.js
@@ -1,39 +1,42 @@
 const handshake = [
-    'wink',
-    'double blink',
-    'close your eyes',
-    'jump'
+    {
+        num: 0b1,
+        text: 'wink'
+    },
+    {
+        num: 0b10,
+        text: 'double blink'
+    },
+    {
+        num: 0b100,
+        text: 'close your eyes'
+    },
+    {
+        num: 0b1000,
+        text: 'jump'
+    }
 ];
 
-function getDigits(n) {
-    if (!Number.isInteger(n)) {
-        throw new Error('Handshake must be a number');
-    }
-    return n
-        .toString(2)
-        .split('')
-        .map(c => Number.parseInt(c, 10))
-        .reverse();
-}
-
-function getCommands(digits) {
-    let commands = [];
-    handshake.forEach((code, index) => {
-        if (digits[index]) {
-            commands.push(code);
-        }
-    });
-    if (digits.length > handshake.length) {
+function makeCommands(n) {
+    let commands = handshake
+        .filter(item => n & item.num)
+        .map(item => {
+            return item.text;
+        });
+    if (n & 0b10000) {
         commands.reverse();
     }
     return commands;
+
 }
 
 class Handshake {
 
     constructor(n) {
-        let digits = getDigits(n);
-        this.output = getCommands(digits);
+        if (!Number.isInteger(n)) {
+            throw new Error('Handshake must be a number');
+        }
+        this.output = makeCommands(n);
     }
 
     commands() {
