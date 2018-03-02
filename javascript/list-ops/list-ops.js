a63c120a9a82b95d00150d834d232b686570c9ab
Pappa <dr.pappa@gmail.com>
2018-03-02 14:23:50 +0000

    Javascript - List Ops

---
+++ javascript/list-ops/list-ops.js
@@ -0,0 +1,73 @@
+class List {
+    constructor(list) {
+        this.values = list || [];
+    }
+
+    length() {
+        return this.values.length;
+    }
+
+    append(list) {
+        while (list.values.length) {
+            this.values[this.length()] = list.values.shift();
+        }
+        return this;
+    }
+
+    concat(list) {
+        this.append(list);
+        return this;
+    }
+
+    filter(predicate) {
+        let results = [];
+        while (this.values.length) {
+            let item = this.values.shift();
+            if (predicate(item)) {
+                results.push(item);
+            }
+        }
+        while (results.length) {
+            this.values.push(results.shift());
+        }
+        return this;
+    }
+
+    map(map) {
+        let results = [];
+        while (this.values.length) {
+            results.push(map(this.values.shift()));
+        }
+        while (results.length) {
+            this.values.push(results.shift());
+        }
+        return this;
+    }
+
+    foldl(reducer, accumulator) {
+        while (this.values.length) {
+            accumulator = reducer(this.values.shift(), accumulator);
+        }
+        return accumulator;
+    }
+
+    foldr(reducer, accumulator) {
+        while (this.values.length) {
+            accumulator = reducer(this.values.pop(), accumulator);
+        }
+        return accumulator;
+    }
+
+    reverse() {
+        let results = this.foldr((val, acc) => {
+            acc.push(val);
+            return acc;
+        }, []);
+        while (results.length) {
+            this.values.push(results.shift());
+        }
+        return this;
+    }
+}
+
+module.exports = List;
