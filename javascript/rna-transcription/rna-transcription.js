080ad8397f90d7cd03a89c65c99f53ec26ff0c9b
Robert Hill <dr.pappa@gmail.com>
2018-03-06 17:59:06 +0000

    Javascript - RNA Transcription

--- javascript/rna-transcription/rna-transcription.js
+++ javascript/rna-transcription/rna-transcription.js
@@ -1,22 +1,24 @@
-var nucleotides = "GCTA".split("");
+var nucleotides = 'GCTA'.split('');
 
 var nucleotideMap = {
-	G: "C",
-	C: "G",
-	T: "A",
-	A: "U"
+	G: 'C',
+	C: 'G',
+	T: 'A',
+	A: 'U'
 };
 
 function isValid(nucleotideArray) {
-	return nucleotideArray.every((n) => nucleotides.includes(n));
+	return nucleotideArray.every(n => nucleotides.includes(n));
 }
 
 function DnaTranscriber() {};
 
 DnaTranscriber.prototype.toRna = (str) => {
-	var nucleotideArray = str.split("");
+	var nucleotideArray = str.split('');
 	if (isValid(nucleotideArray)) {
-		return nucleotideArray.map((n) => nucleotideMap[n]).join();
+		return nucleotideArray
+			.map(n => nucleotideMap[n])
+			.join('');
 	}
 	throw new Error('Invalid input');
 };
