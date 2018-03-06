10d300962ebdece8ddefdf011eff50d4434bc621
Robert Hill <dr.pappa@gmail.com>
2018-03-06 18:07:55 +0000

    Javascript - Leap

--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -1,12 +1,13 @@
-//
-// This is only a SKELETON file for the "Bob" exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+class Year {
 
-var Year = function() {};
+    constructor(input) {
+        this.input = input;
+    }
 
-Year.prototype.isLeap = function(input) {
-    return (input % 4 === 0 && !(input % 100 === 0) || (input % 400 === 0));
-};
+    isLeap() {
+        return (this.input % 4 === 0 && !(this.input % 100 === 0) || (this.input % 400 === 0));
+    }
+
+}
 
 module.exports = Year;
