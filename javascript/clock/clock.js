d016575b048baa29bed2c7f9f7c25408d6e890bb
Robert Hill <dr.pappa@gmail.com>
2018-03-08 19:08:23 +0000

    Javascript - Clock

--- javascript/clock/clock.js
+++ javascript/clock/clock.js
@@ -1,9 +1,8 @@
 class Clock {
 
     constructor(hour, min = 0) {
-        this.time = (hour * 60) + min;
-        this.hour = Math.floor(this.time / 12);
-        this.min = this.time % 60;
+        this.hour = time(hour + Math.floor(min / 60), 24);
+        this.min = time(min, 60);
     }
 
     toString() {
@@ -11,9 +10,21 @@ class Clock {
     }
 
     equals(t2) {
-        return this.time === t2.time;
+        return this.toString() === t2.toString();
+    }
+
+    plus(n) {
+        return new Clock(this.hour, this.min + n);
+    }
+
+    minus(n) {
+        return this.plus(-n);
     }
  }
+
+function time(n, limit) {
+    return n >= 0 ? n % limit : (limit + n % limit) % limit;
+}
 
 function format(t) {
     return ('0' + t).substr(-2);
