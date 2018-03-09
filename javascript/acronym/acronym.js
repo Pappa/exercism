8e677b588747727aa3eee8daa612d95865198286
Robert Hill <dr.pappa@gmail.com>
2018-03-09 18:07:25 +0000

    Javascript - Acronym

--- javascript/acronym/acronym.js
+++ javascript/acronym/acronym.js
@@ -1,11 +1,7 @@
-const regex = /\b([A-Za-z])|[a-z]([A-Z])/g
+const regex = /\W*(\w)([A-Z]+|[a-z]+)\W*/g;
 
 module.exports = {
     parse: str => {
-        let found = '', match;
-        while ((match = regex.exec(str)) !== null) {
-            found += match[1] || match[2];
-        }
-        return found.toUpperCase();
+        return str.replace(regex, '$1').toUpperCase();
     }
 }
