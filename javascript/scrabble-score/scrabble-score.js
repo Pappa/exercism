6865d952c563e8278a4a93e68ef2a7aa0cab21b4
Robert Hill <dr.pappa@gmail.com>
2018-03-11 17:55:04 +0000

    Javascript - Scrabble Score

--- javascript/scrabble-score/scrabble-score.js
+++ javascript/scrabble-score/scrabble-score.js
@@ -1,15 +1,15 @@
 const scores = {
-    1: ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'],
-    2: ['D', 'G'],
-    3: ['B', 'C', 'M', 'P'],
-    4: ['F', 'H', 'V', 'W', 'Y'],
-    5: ['K'],
-    8: ['J', 'X'],
-    10: ['Q', 'Z']
+    1: 'AEIOULNRST',
+    2: 'DG',
+    3: 'BCMP',
+    4: 'FHVWY',
+    5: 'K',
+    8: 'JX',
+    10: 'QZ'
 };
 
 const scoreMap = Object.entries(scores)
-    .reduce((acc, [score, letters]) => {
+    .reduce((acc, [score, [...letters]]) => {
         letters.forEach(l => acc[l] = score);
         return acc;
     }, {});
