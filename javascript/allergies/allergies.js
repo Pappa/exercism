d3d0a3d125b876a0be39c7b679a9868a97104a3f
Pappa <dr.pappa@gmail.com>
2018-03-12 15:54:09 +0000

    Javascript - Allergies

--- javascript/allergies/allergies.js
+++ javascript/allergies/allergies.js
@@ -1,17 +1,13 @@
 const allergens = ['eggs', 'peanuts', 'shellfish', 'strawberries', 'tomatoes', 'chocolate', 'pollen', 'cats'];
 
-const makeBinString = (n, len) => {
-    let str = (n >>> 0).toString(2);
-    return (str.length < len) ? Array(len - str.length).fill('0').join('') + str : str;
-};
-
 class Allergies {
 
     constructor(score) {
-        let binary = makeBinString(score % 256, 8);
-        this.allergies = [...binary].reverse().reduce((acc, bin, idx) => {
-            return Number(bin) ? acc.concat(allergens[idx]) : acc;
-        }, []);
+        this.allergies = [...(score % 256).toString(2)]
+            .reverse()
+            .reduce((acc, bin, idx) => {
+                return Number(bin) ? acc.concat(allergens[idx]) : acc;
+            }, []);
     }
 
     list() {
