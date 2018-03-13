0171a779d3d6a096510bdedcc8c7647c4b1c3bba
Pappa <dr.pappa@gmail.com>
2018-03-13 09:10:34 +0000

    Javascript - Atbash Cipher

--- javascript/atbash-cipher/atbash-cipher.js
+++ javascript/atbash-cipher/atbash-cipher.js
@@ -1,13 +1,17 @@
-const alphabet = 'abcdefghijklmnopqrstuvwxyz';
-const tebahpla = [...alphabet].reverse().join('');
+let alphabet = 'abcdefghijklmnopqrstuvwxyz';
+let tebahpla = [...alphabet].reverse().join('');
+let numbers = '1234567890';
+alphabet = alphabet + numbers;
+tebahpla = tebahpla + numbers;
 
 console.log(alphabet, tebahpla);
 
 const encode = str => {
     return [...str.toLowerCase()]
         .filter(c => alphabet.includes(c))
-        .map((c, idx) => {
-            let space = ((idx + 1) % 5 === 0) ? ' ' : '';
+        .map((c, idx, arr) => {
+            let next = idx + 1;
+            let space = (next % 5 === 0 && arr[next]) ? ' ' : '';
             return tebahpla[alphabet.indexOf(c)] + space;
         })
         .join('');
