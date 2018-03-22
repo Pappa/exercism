189fb79f0438978687ba365dd08a5ca42c88a032
Robert Hill <dr.pappa@gmail.com>
2018-03-22 19:21:37 +0000

    Javascript - Difference of Squares

--- javascript/difference-of-squares/difference-of-squares.js
+++ javascript/difference-of-squares/difference-of-squares.js
@@ -1,11 +1,11 @@
-const range = n => [...Array(n + 1).keys()].slice(1);
+const range = (length, start = 0) => [...Array(length + start).keys()].slice(start);
 const square = n => n * n;
 const sum = (a, b) => a + b;
 
 class Squares {
 
     constructor(n) {
-        let numbers = range(n);
+        let numbers = range(n, 1);
         this.squareOfSums = square(numbers.reduce(sum));
         this.sumOfSquares = numbers.map(square).reduce(sum);
         this.difference = this.squareOfSums - this.sumOfSquares;
