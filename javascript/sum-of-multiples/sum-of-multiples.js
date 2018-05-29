dda3201ea4710d0249307ab070bf63fb085e7a71
Robert Hill <dr.pappa@gmail.com>
2018-05-29 21:09:14 +0100

    Javascript - Sum of Multiples

--- javascript/sum-of-multiples/sum-of-multiples.js
+++ javascript/sum-of-multiples/sum-of-multiples.js
@@ -5,20 +5,10 @@ class SumOfMultiples {
     }
 
     to(limit) {
-        return this.nums
-            .map(n => multiples(n, limit))
-            .reduce(flatten, [])
-            .reduce(unique, [])
-            .reduce(sum, 0);
+        return [...Array(limit).keys()]
+            .filter(x => this.nums.some(n => x % n === 0))
+            .reduce((acc, v) => acc + v, 0);
     }
 }
-
-const multiples = (n, limit) => {
-    return [...Array(limit).keys()]
-        .filter(x => x > 0 && x % n === 0);
-}
-const flatten = (acc, arr) => acc.concat(arr);
-const unique = (acc, v) => acc.includes(v) ? acc : acc.concat(v);
-const sum = (acc, v) => acc + v;
 
 module.exports = SumOfMultiples;
