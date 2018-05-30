985d94a245dcf75d611d73c9c16d7ee83fbc8fcf
Pappa <dr.pappa@gmail.com>
2018-05-30 14:12:21 +0100

    Python - Yacht

--- python/yacht/yacht.py
+++ python/yacht/yacht.py
@@ -1,71 +1,26 @@
 # Score categories
 # Change the values as you see fit
-YACHT = 'YACHT'
+YACHT = lambda  d, ds: 50 if len(ds) == 1 else 0
 ONES = 1
 TWOS = 2
 THREES = 3
 FOURS = 4
 FIVES = 5
 SIXES = 6
-FULL_HOUSE = 'FULL_HOUSE'
-FOUR_OF_A_KIND = 'FOUR_OF_A_KIND'
-LITTLE_STRAIGHT = 'LITTLE_STRAIGHT'
-BIG_STRAIGHT = 'BIG_STRAIGHT'
-CHOICE = 'CHOICE'
+FULL_HOUSE = lambda d, ds: sum(d) if len(ds) == 2 and any(d.count(n) == 3 for n in ds) else 0
+FOUR_OF_A_KIND = lambda d, ds: sum(n * 4 for n in ds if d.count(n) >= 4)
+LITTLE_STRAIGHT = lambda  d, ds: 30 if len(ds) == 5 and 6 not in ds else 0
+BIG_STRAIGHT = lambda  d, ds: 30 if len(ds) == 5 and 1 not in ds else 0
+CHOICE = lambda  d, ds: sum(d)
 
 
 def score(dice, category):
-    dice_set = set(dice)
-    dice_counts = counts(dice)
+    ds = set(dice)
 
-    if category == YACHT:
-        return calculateYacht(dice_set)
-    elif category == CHOICE:
-        return sum(dice)
-    elif category == BIG_STRAIGHT:
-        return calculateBigStraight(dice_set)
-    elif category == LITTLE_STRAIGHT:
-        return calculateLittleStraight(dice_set)
-    elif category in [ONES, TWOS, THREES, FOURS, FIVES, SIXES]:
+    if category in [ONES, TWOS, THREES, FOURS, FIVES, SIXES]:
         return sum([x for x in dice if x == category])
-    elif category == FULL_HOUSE:
-        return calculateFullHouse(dice, dice_set, dice_counts)
-    elif category == FOUR_OF_A_KIND:
-        return calculateFourOfAKind(dice_set, dice_counts)
     else:
-        return 0
-
-
-def calculateYacht(dice_set):
-    if len(dice_set) == 1:
-        return 50
-    else:
-        return 0
-
-def calculateBigStraight(dice_set):
-    if len(dice_set) == 5 and 1 not in dice_set:
-        return 30
-    else:
-        return 0
-
-def calculateLittleStraight(dice_set):
-    if len(dice_set) == 5 and 6 not in dice_set:
-        return 30
-    else:
-        return 0
-
-def calculateFullHouse(dice, dice_set, dice_counts):
-    if len(dice_set) == 2 and all(count > 1 for count in dice_counts.values()):
-        return sum(dice)
-    else:
-        return 0
-
-
-def calculateFourOfAKind(dice_set, dice_counts):
-    if len(dice_set) <= 2 and any(count >= 4 for count in dice_counts.values()):
-        return [n for n, c in dice_counts.items() if c >= 4][0] * 4
-    else:
-        return 0
+        return category(dice, ds)
 
 def counts(dice):
     return  { x: dice.count(x) for x in dice }
