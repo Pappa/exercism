7d572fa14e9f51a4fa5a7e1ebaebcab1babb3615
Pappa <dr.pappa@gmail.com>
2018-05-30 16:13:24 +0100

    WIP - Python - Secret Handshake

--- python/secret-handshake/secret_handshake.py
+++ python/secret-handshake/secret_handshake.py
@@ -1,44 +1,15 @@
-from itertools import dropwhile
-
 def handshake(n):
-    bin_ints = make_bin_ints(n)
-    bin_ints.reverse()
+    bin = n if isinstance(n, int) else int(n, 2)
+    output_commands = [v for k, v in commands.iteritems() if bin & k]
+    return output_commands if bin & 0b10000 else output_commands[::-1]
 
-    output = [handshake_map[i] for i, x in enumerate(bin_ints) if x == 1 and i < 4]
+def code(commands):
+    return 0b0
 
-    if len(bin_ints) < 5:
-        output.reverse()
-    return output
-
-
-def code(handshake):
-	prefix = ''
-	values = handshake_map.values()
-	values.reverse()
-	if not all(h in values for h in handshake):
-		return '0'
-	values_present = [v for v in values if v in handshake]
-
-	print(handshake, values_present)
-	if handshake != values_present:
-		handshake.reverse()
-		prefix = '1'
-	output = [str((0,1)[x in handshake]) for x in values]
-	return prefix + ''.join(output).lstrip('0')
-
-
-def make_bin_ints(n):
-    if isinstance(n, basestring):
-    	return [int(x) for x in n]
-    bin_ints = [int(x) for x in format(n, 'b')]
-    if any(True for x in bin_ints if x < 0 or x > 1):
-        return [0]
-    return bin_ints
-
-handshake_map = {
-    0: "jump",
-    1: "close your eyes",
-    2: "double blink",
-    3: "wink"
+commands = {
+    0b1: 'wink',
+    0b10: 'double blink',
+    0b100: 'close your eyes',
+    0b1000: 'jump'
 }
 
