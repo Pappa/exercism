51e29143e750b972d9bcd9ee1a4a222baf28a34d
Robert Hill <dr.pappa@gmail.com>
2018-05-31 08:21:26 +0100

    WIP - Python - Secret Handshake

--- python/secret-handshake/secret_handshake.py
+++ python/secret-handshake/secret_handshake.py
@@ -1,15 +1,23 @@
 def handshake(n):
-    bin = n if isinstance(n, int) else int(n, 2)
-    output_commands = [v for k, v in commands.iteritems() if bin & k]
-    return output_commands if bin & 0b10000 else output_commands[::-1]
+    if n < 1:
+        return []
+    else:
+        bin = n if isinstance(n, int) else int(n, 2)
+        output_commands = [v for (k, v) in cmd if bin & k]
+        return output_commands if bin & 0b10000 else output_commands[::-1]
 
-def code(commands):
-    return 0b0
+def code(handshake):
+    handshake_commands = [ (k, v) for (k, v) in cmd if v in handshake ]
+    command_value = sum([k for (k, v) in handshake_commands ])
+    ordered_commands = [v for (k, v) in handshake_commands ]
+    if ordered_commands != handshake:
+        command_value += 0b10000
+    print(ordered_commands == handshake, ordered_commands, handshake)
+    print(format(command_value, 'b'))
+    return format(command_value, 'b')
 
-commands = {
-    0b1: 'wink',
-    0b10: 'double blink',
-    0b100: 'close your eyes',
-    0b1000: 'jump'
-}
+
+commands = ['wink', 'double blink', 'close your eyes', 'jump']
+bits = [0b1, 0b10, 0b100, 0b1000]
+cmd = zip(bits, commands)
 
