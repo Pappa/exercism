86ac3beb916aa8fc3d46aaa8e2492b02818eba1b
Pappa <dr.pappa@gmail.com>
2018-05-31 10:15:24 +0100

    Python - Secret Handshake

--- python/secret-handshake/secret_handshake.py
+++ python/secret-handshake/secret_handshake.py
@@ -1,20 +1,28 @@
 def handshake(n):
-    if n < 1:
+    if not is_valid(n):
         return []
     else:
         bin = n if isinstance(n, int) else int(n, 2)
         output_commands = [v for (k, v) in cmd if bin & k]
-        return output_commands if bin & 0b10000 else output_commands[::-1]
+        return output_commands[::-1] if bin & 0b10000 else output_commands
 
 def code(handshake):
+    if (any(h not in commands for h in handshake)):
+        return '0'
     handshake_commands = [ (k, v) for (k, v) in cmd if v in handshake ]
     command_value = sum([k for (k, v) in handshake_commands ])
     ordered_commands = [v for (k, v) in handshake_commands ]
     if ordered_commands != handshake:
         command_value += 0b10000
-    print(ordered_commands == handshake, ordered_commands, handshake)
-    print(format(command_value, 'b'))
     return format(command_value, 'b')
+
+def is_valid(n):
+    if isinstance(n, int) and n > 0:
+        return True
+    elif isinstance(n, str) and all(c in '01' for c in n):
+        return True
+    else:
+        return False
 
 
 commands = ['wink', 'double blink', 'close your eyes', 'jump']
