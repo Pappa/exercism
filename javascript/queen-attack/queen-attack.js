4755ea7cf7061a16bd9fb148bc0c3cc05b72a435
Pappa <dr.pappa@gmail.com>
2018-06-04 15:51:14 +0100

    Javascript - Queen Attack

---
+++ javascript/queen-attack/queen-attack.js
@@ -0,0 +1,36 @@
+class Queens {
+    constructor(pos = { white: [0, 3], black: [7, 3] }) {
+        this.white = pos.white;
+        this.black = pos.black;
+        this.pos = pos;
+        if (!isValid(pos)) {
+            throw new Error('Queens cannot share the same space');
+        }
+    }
+
+    canAttack() {
+        return (
+            canAttackOrthogonally(this.pos)
+            || canAttackDiagonally(this.pos)
+        )
+    }
+
+    toString() {
+        let chessboard = Array(8).fill().map(_ => Array(8).fill('_'));
+        chessboard[this.white[0]][this.white[1]] = 'W';
+        chessboard[this.black[0]][this.black[1]] = 'B';
+        return chessboard.map(row => row.join(' ')).join('\n') + '\n';
+    }
+}
+
+const canAttackOrthogonally = ({ white, black }) => white.some((v, idx) => v === black[idx]);
+
+const canAttackDiagonally = ({ white, black }) => {
+    const deltaX = Math.abs(white[0] - black[0]);
+    const deltaY = Math.abs(white[1] - black[1]);
+    return deltaY / deltaX === 1;
+};
+
+const isValid = ({ white, black }) => white.some((v, idx) => v !== black[idx]);
+
+module.exports = Queens;
