ace63baf87ad67ba02d1e09ee852ff096b176b4c
Pappa <dr.pappa@gmail.com>
2018-07-17 08:07:55 +0100

    C# - Leap

--- csharp/leap/Leap.cs
+++ csharp/leap/Leap.cs
@@ -4,9 +4,8 @@ public static class Leap
 {
     public static bool IsLeapYear(int year)
     {
-        return (
+        return 
             year % 4 == 0 
-            && (year % 100 != 0) || (year % 400 == 0)
-        );
+            && (year % 100 != 0 || year % 400 == 0);
     }
 }
