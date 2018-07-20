8faf97a9886df6c288da4fe132acb1bf42cbcd9a
Pappa <dr.pappa@gmail.com>
2018-07-20 11:09:59 +0100

    C# - Reverse String

---
+++ csharp/reverse-string/ReverseString.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public static class ReverseString
+{
+    public static string Reverse(string input)
+    {
+        char[] letters = input.ToCharArray();
+        Array.Reverse(letters);
+        return new String(letters);
+    }
+}
