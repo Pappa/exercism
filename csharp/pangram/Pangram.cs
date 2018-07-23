bd157aa5f0ffac0958295f8d6b8f891348a679d8
Pappa <dr.pappa@gmail.com>
2018-07-23 20:25:48 +0100

    C# - Pangram

---
+++ csharp/pangram/Pangram.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+public static class Pangram
+{
+    public static bool IsPangram(string input) =>
+        UniqueLetters(input).Length == 26;
+
+    private static char[] UniqueLetters(string input) => 
+        Regex.Replace(input, @"[^a-zA-Z]", "")
+            .ToUpper()
+            .ToCharArray()
+            .Distinct()
+            .ToArray();
+}
