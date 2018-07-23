cceb1c1811aa6fec29d1c2807eaf99e91d1db587
Pappa <dr.pappa@gmail.com>
2018-07-23 19:44:44 +0100

    C# - RNA Transcription

---
+++ csharp/rna-transcription/RnaTranscription.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public static class RnaTranscription
+{
+    private static Dictionary<string, string> data = new Dictionary<string, string>
+    {
+        { "G", "C" }, 
+        { "C", "G" }, 
+        { "T", "A" }, 
+        { "A", "U" }
+    };
+
+    public static string ToRna(string nucleotide)
+    {
+        var transposed = nucleotide.Select(Transpose).ToArray();
+        return String.Join("", transposed);
+    }
+
+    private static string Transpose(char c) => data[c.ToString()];
+}
