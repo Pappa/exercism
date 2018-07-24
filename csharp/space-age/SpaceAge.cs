7f8acdaf7c54459821dbe7d4de57a91b54ed6c9d
Pappa <dr.pappa@gmail.com>
2018-07-24 21:14:56 +0100

    C# - Space Age

--- csharp/space-age/SpaceAge.cs
+++ csharp/space-age/SpaceAge.cs
@@ -2,38 +2,38 @@ using System;
 
 public class SpaceAge
 {
-    private const float EARTH_YEAR_IN_SEC = 31557600;
-    private float age;
+    private const double EARTH_YEAR_IN_SEC = 31557600;
+    private double age;
 
     public SpaceAge(long seconds)
     {
-        age = (float)seconds;
+        age = (double)seconds;
     }
 
     public double OnEarth() =>
         AgeFromPeriod();
 
     public double OnMercury() =>
-        AgeFromPeriod((float)0.2408467);
+        AgeFromPeriod((double)0.2408467);
 
     public double OnVenus() =>
-        AgeFromPeriod((float)0.61519726);
+        AgeFromPeriod((double)0.61519726);
 
     public double OnMars() =>
-        AgeFromPeriod((float)1.8808158);
+        AgeFromPeriod((double)1.8808158);
 
     public double OnJupiter() =>
-        AgeFromPeriod((float)11.862615);
+        AgeFromPeriod((double)11.862615);
 
     public double OnSaturn() =>
-        AgeFromPeriod((float)29.447498);
+        AgeFromPeriod((double)29.447498);
 
     public double OnUranus() =>
-        AgeFromPeriod((float)84.016846);
+        AgeFromPeriod((double)84.016846);
 
     public double OnNeptune() =>
-        AgeFromPeriod((float)164.79132);
+        AgeFromPeriod((double)164.79132);
 
-    private double AgeFromPeriod(float period = 1) =>
-        (double)(age / (EARTH_YEAR_IN_SEC * period));
+    private double AgeFromPeriod(double period = 1) =>
+        age / EARTH_YEAR_IN_SEC / period;
 }
