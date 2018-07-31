0cd028987473051fee9c37db3c804c6ab03f5d17
Pappa <dr.pappa@gmail.com>
2018-07-31 08:40:10 +0100

    C# - Gigasecond

--- csharp/gigasecond/Gigasecond.cs
+++ csharp/gigasecond/Gigasecond.cs
@@ -2,9 +2,7 @@ using System;
 
 public static class Gigasecond
 {
-    private const double gigasecond = 1e9;
-    public static DateTime Add(DateTime birthDate)
-    {
-        return birthDate.AddSeconds(gigasecond);
-    }
+    private const double GigaSecond = 1e9;
+    public static DateTime Add(DateTime birthDate) => 
+        birthDate.AddSeconds(GigaSecond);
 }
