d19b61682108757ba1b0ca49a6a5777014b4d68d
Pappa <dr.pappa@gmail.com>
2018-07-31 16:44:41 +0100

    C# - Error Handling

---
+++ csharp/error-handling/ErrorHandling.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+public static class ErrorHandling
+{
+    public static void HandleErrorByThrowingException() =>
+        throw new Exception();
+
+    public static int? HandleErrorByReturningNullableType(string input)
+    {
+        if (int.TryParse(input, out int output))
+        {
+            return output;
+        }
+        return null;
+    }
+
+    public static bool HandleErrorWithOutParam(string input, out int result) =>
+        int.TryParse(input, out result);
+
+    public static void DisposableResourcesAreDisposedWhenExceptionIsThrown(IDisposable disposableObject)
+    {
+        disposableObject.Dispose();
+        throw new Exception();
+    }
+}
