091f34cb23ff23906f38864ee2cfa6f0635862f9
Pappa <dr.pappa@gmail.com>
2018-08-01 10:28:18 +0100

    F# - Leap Year - Using Parameterised Active Pattern

--- fsharp/leap/Leap.fs
+++ fsharp/leap/Leap.fs
@@ -1,4 +1,10 @@
 ﻿module Leap
 
+let (|DivisibleBy|_|) x y = if y % x = 0 then Some() else None
+
 let leapYear (year: int): bool = 
-    year % 4 = 0 && (year % 100 <> 0 || year % 400 = 0);
+    match year with
+    | DivisibleBy 400 year -> true
+    | DivisibleBy 100 year -> false
+    | DivisibleBy 4 year   -> true
+    | _                    -> false
