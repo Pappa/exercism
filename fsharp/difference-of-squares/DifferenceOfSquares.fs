bf7cd8aae13febfd20baf96043927c79a44d6f61
Pappa <dr.pappa@gmail.com>
2018-08-02 17:02:36 +0100

    F# - Difference of Squares

---
+++ fsharp/difference-of-squares/DifferenceOfSquares.fs
@@ -0,0 +1,14 @@
+﻿module DifferenceOfSquares
+
+open System
+
+let square (n: int): int = n * n
+
+let squareOfSum (n: int): int = 
+    square <| List.sum [1 .. n]
+
+let sumOfSquares (n: int): int = 
+    List.sumBy square [1 .. n]
+
+let differenceOfSquares (n: int): int = 
+    abs <| squareOfSum n - sumOfSquares n
