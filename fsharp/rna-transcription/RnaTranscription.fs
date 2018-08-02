9f85ae2dfcda210c4e6081b0cb689002aed325e9
Pappa <dr.pappa@gmail.com>
2018-08-02 21:47:52 +0100

    F# - RNA Transcription

---
+++ fsharp/rna-transcription/RnaTranscription.fs
@@ -0,0 +1,20 @@
+﻿module RnaTranscription
+
+open System
+
+let NucleotideMap =
+    [ 
+        'G', "C";
+        'C', "G";
+        'T', "A";
+        'A', "U"
+    ]
+    |> Map.ofList
+
+let transcribe (c: char): string = 
+    match c |> NucleotideMap.TryFind with
+    | Some x -> x
+    | None -> ""
+
+let toRna (dna: string): string = 
+    String.collect transcribe dna
