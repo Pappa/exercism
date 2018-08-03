16b17ffa542649959f76ee4130cfc4e33516858e
Pappa <dr.pappa@gmail.com>
2018-08-03 10:57:32 +0100

    F# - Pangram

---
+++ fsharp/pangram/Pangram.fs
@@ -0,0 +1,13 @@
+﻿module Pangram
+
+open System
+
+let uniqueLetters (input: string): string =
+    input.ToUpper()
+    |> Seq.toArray
+    |> Array.filter Char.IsLetter
+    |> Array.distinct
+    |> String
+
+let isPangram (input: string): bool = 
+    uniqueLetters(input).Length = 26
