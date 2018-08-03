43ec53fa8076295bd1b14a41189c93868c6c5d79
Pappa <dr.pappa@gmail.com>
2018-08-03 22:51:33 +0100

    WIP - F# - Grains

---
+++ fsharp/grains/Grains.fs
@@ -0,0 +1,16 @@
+﻿module Grains
+
+open System.Numerics
+
+let square (n: int): Result<uint64,string> = 
+    match n with
+    | n when n > 0 && n < 65 -> 
+        let value = pown 2 (n - 1)
+        uint64(value) |> Ok
+    | _ -> Error "Invalid input"
+
+// let total: Result<uint64,string> = 
+//     square 64
+//     |> (-) 1
+
+let total: Result<uint64,string> = failwith "You need to implement this function."
