8d5c0f12f3983421fd5872cf5866ad944fe357eb
Pappa <dr.pappa@gmail.com>
2018-08-03 16:54:30 +0100

    F# - Isogram

---
+++ fsharp/isogram/Isogram.fs
@@ -0,0 +1,12 @@
+﻿module Isogram
+
+open System
+
+let isIsogram (str: string): bool = 
+    let letters = 
+        str.ToUpper()
+        |> String.filter Char.IsLetter
+
+    Seq.distinct letters
+    |> Seq.length
+    |> (=) letters.Length
