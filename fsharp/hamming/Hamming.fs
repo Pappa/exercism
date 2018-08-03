ac926063bf37e675e442a0e234398ba808f7e47c
Pappa <dr.pappa@gmail.com>
2018-08-03 09:38:22 +0100

    F# - Hamming Distance

---
+++ fsharp/hamming/Hamming.fs
@@ -0,0 +1,11 @@
+﻿module Hamming
+
+let countIfDifferent ((a, b): char * char): int = 
+    if a <> b then 1 else 0
+
+let distance (s1: string) (s2: string): int option = 
+    if s1.Length <> s2.Length then None else
+        Seq.zip s1 s2
+        |> Seq.map countIfDifferent
+        |> Seq.sum
+        |> Some
