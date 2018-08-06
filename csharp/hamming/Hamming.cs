75efa5e7b43e38304a4f1f853c6821b3bcb6f74e
Pappa <dr.pappa@gmail.com>
2018-08-06 13:26:57 +0100

    C# - Hamming Distance

--- csharp/hamming/Hamming.cs
+++ csharp/hamming/Hamming.cs
@@ -6,9 +6,11 @@ public static class Hamming
 {
     public static int Distance(string first, string second)
     {
-        if (first.Length != second.Length) {
+        if (first.Length != second.Length)
+        {
             throw new ArgumentException();
         }
+        
         return first.ToCharArray()
             .Zip(second.ToCharArray(), CountIfDifferent)
             .Sum();
