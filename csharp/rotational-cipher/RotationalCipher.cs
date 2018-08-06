2704feafee2af14ee35ed5d6c33b23e08a81c940
Pappa <dr.pappa@gmail.com>
2018-08-06 13:27:31 +0100

    C# - Rotational Cipher

--- csharp/rotational-cipher/RotationalCipher.cs
+++ csharp/rotational-cipher/RotationalCipher.cs
@@ -10,9 +10,11 @@ public static class RotationalCipher
         string.Join("", text.Select(c => Rotate(c, key)));
 
     private static char Rotate(char c, int key) {
-        if (!Char.IsLetter(c)) {
+        if (!Char.IsLetter(c))
+        {
             return c;
         }
+        
         string alphabet = Char.IsLower(c) ? Lower : Upper;
         int idx = alphabet.IndexOf(c);
         return alphabet[(idx + key) % alphabet.Length];
