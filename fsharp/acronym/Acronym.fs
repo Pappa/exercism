10fad265bc846300237ac7b58d3d2c5770e2e24e
Pappa <dr.pappa@gmail.com>
2018-08-07 13:40:40 +0100

    F# - Acronym

---
+++ fsharp/acronym/Acronym.fs
@@ -0,0 +1,8 @@
+﻿module Acronym
+
+open System
+
+let abbreviate (phrase: string): string = 
+    phrase.ToUpper().Split([|" "; "-"|], StringSplitOptions.None)
+    |> Array.map (fun w -> w.[0])
+    |> String
