5a574897968085e72625192021b6717867fe4191
Pappa <dr.pappa@gmail.com>
2018-08-07 14:46:15 +0100

    F# - Sum of Multiples

---
+++ fsharp/sum-of-multiples/SumOfMultiples.fs
@@ -0,0 +1,12 @@
+﻿module SumOfMultiples
+
+open System
+
+let multiples (max: int) (n: int) : int list = 
+    [ for i in n .. n .. (max - 1) -> i ]
+
+let sum (numbers: int list) (max: int): int = 
+    numbers
+    |> List.collect (multiples max)
+    |> Seq.distinct
+    |> Seq.sum 
