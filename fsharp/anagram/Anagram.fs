e66e684dfc6ba0543a24e00b0d3760c1e360feac
Pappa <dr.pappa@gmail.com>
2018-08-07 13:20:15 +0100

    F# - Anagram

---
+++ fsharp/anagram/Anagram.fs
@@ -0,0 +1,12 @@
+﻿module Anagram
+
+open System
+
+let sort (input: string): string = input.ToLower() |> Seq.sort |> Array.ofSeq |> String
+
+let isAnagram (s1: string) (s2: string): bool =
+    s1.ToLower() <> s2.ToLower() &&
+    (sort s1) = (sort s2)
+
+let anagrams (sources: string list) (target: string): string list = 
+    sources |> List.filter (isAnagram target)
