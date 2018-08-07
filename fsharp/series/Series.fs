eea6b0ec594fced4a190aeff52c91929667a2e07
Pappa <dr.pappa@gmail.com>
2018-08-07 12:57:41 +0100

    F# - Series

---
+++ fsharp/series/Series.fs
@@ -0,0 +1,9 @@
+﻿module Series
+
+let slices (str: string) (length: int): string list option = 
+    if str.Length < 1 || length < 1 || length > str.Length
+    then None
+    else
+        [0 .. str.Length - length]
+        |> List.mapi (fun n i -> str.[i .. i + length - 1])
+        |> Some
