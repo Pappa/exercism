b367cde74d82a11fc107c96e4c44e380b939e383
Pappa <dr.pappa@gmail.com>
2018-08-08 08:42:48 +0100

    F# - Collatz Conjecture

---
+++ fsharp/collatz-conjecture/CollatzConjecture.fs
@@ -0,0 +1,19 @@
+﻿module CollatzConjecture
+
+
+
+let rec collatz (n: int) = seq {
+    match n with
+    | 1 -> do ()
+    | x when x % 2 = 0 -> 
+        yield x
+        yield! collatz (x / 2)
+    | _ -> 
+        yield n
+        yield! collatz (n * 3 + 1)
+}
+
+let steps (number: int): int option = 
+    match number with
+    | x when x < 1 -> None
+    | _ -> collatz number |> Seq.length |> Some
