b7bca4aa2079e428dcb19270e4acf359aac08d7f
Pappa <dr.pappa@gmail.com>
2018-08-08 08:13:34 +0100

    F# - Two Fer

---
+++ fsharp/two-fer/TwoFer.fs
@@ -0,0 +1,9 @@
+﻿module TwoFer
+
+let twoFer (input: string option): string = 
+    let who = 
+        match input with
+        | Some x -> x
+        | None -> "you"
+
+    sprintf "One for %s, one for me." who
