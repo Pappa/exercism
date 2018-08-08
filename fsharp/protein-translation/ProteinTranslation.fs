d6cb02fb7153e195032fb704c318da1a1f76603e
Pappa <dr.pappa@gmail.com>
2018-08-08 11:59:01 +0100

    F# - Protein Translation

--- fsharp/protein-translation/ProteinTranslation.fs
+++ fsharp/protein-translation/ProteinTranslation.fs
@@ -16,15 +16,10 @@ let private CODONS_BY_PROTEIN =
 let getProtein (codon: string): string =
     Map.fold (fun state protein codons -> if List.contains codon codons then protein else state) "" CODONS_BY_PROTEIN
 
-let slices (str: string) (length: int): string list = 
-    if str.Length < 1 || length < 1 || length > str.Length
-    then []
-    else
-        [0 .. length .. str.Length - length]
-        |> List.map (fun i -> str.[i .. i + length - 1])
-
 let proteins (rna: string): string list = 
-    slices rna 3
+    let length = 3
+    [0 .. length .. rna.Length - length]
+    |> List.map (fun i -> rna.[i .. i + length - 1])
     |> Seq.takeWhile (fun c -> not <| List.contains c STOP_CODONS) 
     |> List.ofSeq
     |> List.map getProtein
