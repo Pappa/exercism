85a727c1708ff792c639aa5550e4a22edf03500b
Pappa <dr.pappa@gmail.com>
2018-08-08 09:36:07 +0100

    F# - Armstrong Numbers

--- fsharp/armstrong-numbers/ArmstrongNumbers.fs
+++ fsharp/armstrong-numbers/ArmstrongNumbers.fs
@@ -2,8 +2,11 @@
 
 open System
 
+let toDigits (number: int): int list = 
+    number |> string |> Seq.toList |> List.map Char.GetNumericValue |> List.map int
+
 let isArmstrongNumber (number: int): bool = 
-    let (digits: int list) = number |> string |> Seq.toList |> List.map Char.GetNumericValue |> List.map int
+    let digits = toDigits number
     let pow = digits.Length
 
     List.sum [ for d in digits -> pown d pow ] |> (=) number
