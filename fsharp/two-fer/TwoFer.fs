4c2e83a4b76a6a8cae3e86f96fb35a191fb1afd7
Pappa <dr.pappa@gmail.com>
2018-08-08 13:23:25 +0100

    F# - Two Fer

--- fsharp/two-fer/TwoFer.fs
+++ fsharp/two-fer/TwoFer.fs
@@ -1,9 +1,6 @@
 ﻿module TwoFer
 
 let twoFer (input: string option): string = 
-    let who = 
-        match input with
-        | Some x -> x
-        | None -> "you"
-
+    let who = defaultArg input "you"
+    
     sprintf "One for %s, one for me." who
