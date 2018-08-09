8a1cfa3195e3fa4128ad7dd491cd6d213435bccf
Pappa <dr.pappa@gmail.com>
2018-08-09 22:01:41 +0100

    WIP - F# - Binary Search Tree

---
+++ fsharp/binary-search-tree/BinarySearchTree.fs
@@ -0,0 +1,27 @@
+﻿module BinarySearchTree
+
+open System
+
+type BST<'a> =
+    | Leaf
+    | Node of BST<'a> * 'a * BST<'a>
+
+let private simgleton (value: 'a): BST<'a> = 
+    Node (Leaf, value, Leaf)
+
+let left node = function
+    | Node (x, _, _) -> Some x
+    | Leaf -> None
+
+let right node = function
+    | Node (_, _, x) -> Some x
+    | Leaf -> None
+
+let data node = function 
+    | Node (_, x, _) -> x
+    | _ -> failwith "Oops"
+
+let create (items: 'a list): BST<'a> =
+    simgleton items.[0]
+
+let sortedData node = failwith "You need to implement this function."
