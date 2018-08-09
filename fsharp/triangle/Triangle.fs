5e5773e11aea0118969f953c15828203b681cf61
Pappa <dr.pappa@gmail.com>
2018-08-09 10:56:33 +0100

    F# - Triangle

---
+++ fsharp/triangle/Triangle.fs
@@ -0,0 +1,18 @@
+﻿module Triangle
+
+open System
+
+let isValid (triangle: float list): bool = 
+    match List.sort triangle with
+    | [a; b; c] ->
+        a + b >= c && List.forall (fun side -> side > 0.0) triangle
+    | _ -> false
+
+let equilateral (triangle: float list): bool = 
+    isValid triangle && Seq.distinct triangle |> Seq.length |> (=) 1
+
+let isosceles (triangle: float list): bool =
+    isValid triangle && Seq.distinct triangle |> Seq.length |> (>=) 2
+
+let scalene (triangle: float list): bool =
+    isValid triangle && Seq.distinct triangle |> Seq.length |> (=) 3
