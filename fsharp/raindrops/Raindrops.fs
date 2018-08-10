d337b345a23a2f7080b915002da9dd71755b1eb6
Pappa <dr.pappa@gmail.com>
2018-08-10 11:58:50 +0100

    F# - Raindrops

---
+++ fsharp/raindrops/Raindrops.fs
@@ -0,0 +1,16 @@
+﻿module Raindrops
+
+open System
+
+let raindrops = [(3, "Pling"); (5, "Plang"); (7, "Plong")]
+
+let getRaindrop (number: int) (factor, str) =
+    if (number % factor = 0)
+    then str
+    else "" 
+
+let convert (number: int): string = 
+    let str = List.map (getRaindrop number) raindrops |> String.concat ""
+    match str with
+    | x when x.Length > 0 -> x
+    | _ -> string number
