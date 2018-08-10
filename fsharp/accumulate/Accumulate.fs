fe2b5501141adb893c5d40ab1b65ea436967dcea
Pappa <dr.pappa@gmail.com>
2018-08-10 15:05:30 +0100

    F# - Accumulate

---
+++ fsharp/accumulate/Accumulate.fs
@@ -0,0 +1,9 @@
+﻿module Accumulate
+
+let rec private accumulator<'a, 'b> (func: 'a -> 'b) (input: 'a list) (acc: 'b list): 'b list = 
+    match input with
+    | x::xs -> accumulator func xs (acc @ [func x])
+    | [] -> acc
+
+let accumulate<'a, 'b> (func: 'a -> 'b) (input: 'a list): 'b list = 
+    accumulator func input []
