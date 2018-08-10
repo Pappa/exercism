e33b48eb6705cb718f3a5b5865d9ca1b92ed0ff3
Pappa <dr.pappa@gmail.com>
2018-08-10 14:31:24 +0100

    F# - Raindrops

--- fsharp/raindrops/Raindrops.fs
+++ fsharp/raindrops/Raindrops.fs
@@ -2,13 +2,7 @@
 
 let raindrops = [(3, "Pling"); (5, "Plang"); (7, "Plong")]
 
-let getRaindrop (number: int) (factor, str) =
-    if (number % factor = 0)
-    then str
-    else "" 
-
 let convert (number: int): string = 
-    let str = List.map (getRaindrop number) raindrops |> String.concat ""
-    match str with
-    | x when x.Length > 0 -> x
-    | _ -> string number
+    raindrops 
+    |> List.fold (fun acc (factor, str) -> if number % factor = 0 then acc + str else acc) ""
+    |> (fun str -> if str.Length > 0 then str else string number) 
