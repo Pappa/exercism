759b60fcb34d8b44113b547d831264fd2e848f5a
Pappa <dr.pappa@gmail.com>
2018-08-10 10:49:16 +0100

    WIP - F# - Binary Search Tree

--- fsharp/binary-search-tree/BinarySearchTree.fs
+++ fsharp/binary-search-tree/BinarySearchTree.fs
@@ -6,19 +6,20 @@ type BST<'a> =
     | Leaf
     | Node of BST<'a> * 'a * BST<'a>
 
-let private simgleton (value: 'a): BST<'a> = Node (Leaf, value, Leaf)
+let private simgleton (value: 'a): BST<'a> = 
+    Node (Leaf, value, Leaf)
 
 let left node = 
     match node with
     | Node (Leaf, _, _) -> None
     | Node (x, _, _) -> Some x
-    | Leaf -> None
+    | _ -> failwith "Oops"
 
 let right node = 
     match node with
     | Node (_, _, Leaf) -> None
     | Node (_, _, x) -> Some x
-    | Leaf -> None
+    | _ -> failwith "Oops"
 
 let data node = 
     match node with
