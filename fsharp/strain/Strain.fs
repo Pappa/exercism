c5f55c6084bb4821a73b4b65c6408bc95fd21d13
Pappa <dr.pappa@gmail.com>
2018-08-13 15:46:09 +0100

    F# - Strain

--- fsharp/strain/Strain.fs
+++ fsharp/strain/Strain.fs
@@ -1,7 +1,8 @@
 ﻿module Seq
 
 let keep predicate xs = 
-    [for x in xs do if predicate x then yield x] |> Seq.ofList
+    [for x in xs do if predicate x then yield x]
+    |> Seq.ofList
 
 let discard predicate xs = 
     keep (predicate >> not) xs
