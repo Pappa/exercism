c7fb36f15b5f0b4d4c9e3773aa21f39af7a24538
Pappa <dr.pappa@gmail.com>
2018-08-14 15:49:31 +0100

    F# - Grade School

---
+++ fsharp/grade-school/GradeSchool.fs
@@ -0,0 +1,20 @@
+﻿module GradeSchool
+
+open System
+
+type School = Map<int, string list>
+
+let grade (n: int) (school: School): string list = 
+    let students = school.TryFind(n)
+    match students with
+    | Some x -> x
+    | None -> []
+
+let empty: School = Map.empty
+
+let add (student: string) (n: int) (school: School): School = 
+    let students = grade n school
+    school |> Map.add n (List.sort (student :: students))
+
+let roster (school: School): (int * string list) list = 
+    school |> Map.toSeq |> Seq.toList
