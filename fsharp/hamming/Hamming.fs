e7264c6d451001421ed997bbaf50103ed499af77
Pappa <dr.pappa@gmail.com>
2018-08-18 15:05:14 +0100

    F# - Hamming

--- fsharp/hamming/Hamming.fs
+++ fsharp/hamming/Hamming.fs
@@ -1,11 +1,21 @@
 ﻿module Hamming
 
-let countIfDifferent ((a, b): char * char): int = 
+let countIfDifferent (a, b): int = 
     if a <> b then 1 else 0
 
 let distance (s1: string) (s2: string): int option = 
     if s1.Length <> s2.Length then None else
         Seq.zip s1 s2
-        |> Seq.map countIfDifferent
-        |> Seq.sum
+        |> Seq.sumBy countIfDifferent
         |> Some
+
+// let different (a, b) = 
+//     if a <> b then Some (a, b) else None
+
+// let distance (s1: string) (s2: string): int option = 
+//     if s1.Length <> s2.Length then None else
+//         Seq.zip s1 s2
+//         |> Seq.map different
+//         |> Seq.choose id
+//         |> Seq.length
+//         |> Some
