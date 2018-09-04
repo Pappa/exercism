836161823d934919fc4922721dba0eb9d23dffe3
Pappa <dr.pappa@gmail.com>
2018-09-04 17:54:15 +0100

    Haskell - Hamming Distance

--- haskell/hamming/src/Hamming.hs
+++ haskell/hamming/src/Hamming.hs
@@ -5,4 +5,7 @@ distance s1 s2
     | length s1 /= length s2 = Nothing
     | s1 == s2 = Just 0
     | otherwise = Just dist 
-    where dist = fromIntegral $ length $ filter (\(a, b) -> a /= b) (zip s1 s2)
+    where dist = fromIntegral $ length $ filter different (zip s1 s2)
+
+different :: (Char, Char) -> Bool
+different (a, b) = a /= b
