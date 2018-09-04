c13cb4bb3c6780fe205652326bdf747dc53fd130
Pappa <dr.pappa@gmail.com>
2018-09-04 17:57:34 +0100

    Haskell - Hamming Distance

--- haskell/hamming/src/Hamming.hs
+++ haskell/hamming/src/Hamming.hs
@@ -1,11 +1,12 @@
 module Hamming (distance) where
+import Data.List (genericLength)
 
 distance :: String -> String -> Maybe Integer
 distance s1 s2
     | length s1 /= length s2 = Nothing
     | s1 == s2 = Just 0
     | otherwise = Just dist 
-    where dist = fromIntegral $ length $ filter different (zip s1 s2)
+    where dist = genericLength $ filter different (zip s1 s2)
 
 different :: (Char, Char) -> Bool
 different (a, b) = a /= b
