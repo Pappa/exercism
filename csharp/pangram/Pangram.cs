833b0815ec3e4de03db9301cfb7cef5a7d029911
Pappa <dr.pappa@gmail.com>
2018-09-18 19:17:07 +0100

    C# - Pangram

--- csharp/pangram/Pangram.cs
+++ csharp/pangram/Pangram.cs
@@ -6,11 +6,10 @@ using System.Text.RegularExpressions;
 public static class Pangram
 {
     public static bool IsPangram(string input) =>
-        UniqueLetters(input).Length == 26;
+        UniqueLetters(input).Count() == 26;
 
-    private static char[] UniqueLetters(string input) => 
+    private static IEnumerable<char> UniqueLetters(string input) => 
         input.Where(c => Char.IsLetter(c))
             .Select(c => Char.ToUpper(c))
-            .Distinct()
-            .ToArray();
+            .Distinct();
 }
