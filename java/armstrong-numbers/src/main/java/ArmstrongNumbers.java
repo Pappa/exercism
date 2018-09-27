db1d8b32ae2602371a6059f3762002cf6079b618
Pappa <dr.pappa@gmail.com>
2018-09-27 18:32:09 +0100

    Java - Armstrong Numbers

---
+++ java/armstrong-numbers/src/main/java/ArmstrongNumbers.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.*;
+
+class ArmstrongNumbers {
+
+	IntStream getDigits(String n) {
+		return n.chars().map(c -> Character.getNumericValue(c));
+	}
+
+	boolean isArmstrongNumber(int n) {
+		String str = Integer.toString(n);
+		double pow = str.length();
+		return n == str
+			.chars()
+			.map(c -> Character.getNumericValue(c))
+			.map(d -> (int)Math.pow((double)d, pow))
+			.sum();
+	}
+
+}
