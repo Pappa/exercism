c6a025471f9bc99e7da5095dee385b7fa63b2256
Pappa <dr.pappa@gmail.com>
2018-09-27 19:17:11 +0100

    Java - Armstrong Numbers

--- java/armstrong-numbers/src/main/java/ArmstrongNumbers.java
+++ java/armstrong-numbers/src/main/java/ArmstrongNumbers.java
@@ -2,17 +2,17 @@ import java.util.stream.*;
 
 class ArmstrongNumbers {
 
-	IntStream getDigits(String n) {
+	int getSumOfPowers(String n, double pow) {
 		return n.chars()
-			.map(c -> Character.getNumericValue(c));
+			.map(c -> Character.getNumericValue(c))
+			.map(d -> (int)Math.pow((double)d, pow))
+			.sum();
 	}
 
 	boolean isArmstrongNumber(int n) {
 		String str = Integer.toString(n);
 		double pow = str.length();
-		return n == this.getDigits(str)
-			.map(d -> (int)Math.pow((double)d, pow))
-			.sum();
+		return n == this.getSumOfPowers(str, pow);
 	}
 
 }
