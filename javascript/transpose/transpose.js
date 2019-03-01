a8b5cb094bd00586c399e5513236ca769d9a5254
Pappa <dr.pappa@gmail.com>
2019-03-01 16:15:29 +0000

    Javascript - Transpose

--- javascript/transpose/transpose.js
+++ javascript/transpose/transpose.js
@@ -1,11 +1,9 @@
 export const transpose = input => {
   const rows = input.map(c => [...c]);
-  if (!rows.length) {
-    return [];
-  }
-  const result = Array.from({ length: Math.max(...rows.map(r => r.length)) })
-    .map((_, i) => rows.map(x => x[i] || " "))
-    .map(r => r.join(""));
-  result[result.length - 1] = result[result.length - 1].trimRight();
-  return result;
+  return Array.from({
+    length: Math.max(...rows.map(r => r.length))
+  }).map((_, i, arr) => {
+    const r = rows.map(x => x[i] || " ").join("");
+    return i < arr.length - 1 ? r : r.trimRight();
+  });
 };
